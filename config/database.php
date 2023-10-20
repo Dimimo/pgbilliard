40e8c199331ca9cd41fbf772e84be5d9d1806ec9
Dimitri Mostrey <dmostrey@yahoo.com>
2023-10-20 16:09:18 +0800

    Config files, including root. Composer, package, tailwind, vite.

--- config/database.php
+++ config/database.php
@@ -63,6 +63,22 @@ return [
             ]) : [],
         ],
 
+        'parrot' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_PARROT_DATABASE', 'forge'),
+            'username' => env('DB_PARROT_USERNAME', 'forge'),
+            'password' => env('DB_PARROT_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
