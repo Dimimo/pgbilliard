df10d4c78efab0a27074529a8c5f3cbf0b9ea40a
Dimitri Mostrey <dmostrey@yahoo.com>
2023-10-23 10:59:11 +0800

    dropped the volt component part for it was empty

--- resources/views/pages/admin/db.blade.php
+++ resources/views/pages/admin/db.blade.php
@@ -1,14 +1,3 @@
-<?php
-
-use Livewire\Volt\Component;
-use function Laravel\Folio\{middleware};
-
-middleware(['web']);
-
-new class extends Component {
-    //
-}
-?>
 <x-layout>
     @volt()
     <div>
