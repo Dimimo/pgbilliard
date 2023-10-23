6b43dfd49510cc05ec438bed9b95b6bbf476f193
Dimitri Mostrey <dmostrey@yahoo.com>
2023-10-23 10:59:46 +0800

    Team member overview, shows empty if no players are added

--- resources/views/livewire/players/overview.blade.php
+++ resources/views/livewire/players/overview.blade.php
@@ -1,5 +1,5 @@
 <div class="mt-4">
-    @foreach ($players as $player)
+    @forelse ($players as $player)
         <div class="flex justify-center gap-4" wire:key="{{ $player->id }}">
             <div>
                 <x-captain :player="$player"/>
@@ -7,8 +7,10 @@
             <div class="text-xl">{{ $player->name }}</div>
             <div class="text-xl">{{ $player->contact_nr }}</div>
         </div>
-    @endforeach
-    @if ($hasAccess)
+    @empty
+        <div class="flex justify-center text-xl">No known selected players in this team</div>
+    @endforelse
+    @can ('update', $team)
         <a class="flex justify-end p-4" href="/admin/teams/edit/{{  $team->id }}" wire:navigate>
             <div class="mr-2">
                 <img src="{{ secure_asset('svg/pen-square.svg') }}" alt="" width="24" height="24">
