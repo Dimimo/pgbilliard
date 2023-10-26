4932a863fd667f2740cf4e337c20798af4d97e04
Dimitri Mostrey <dmostrey@yahoo.com>
2023-10-26 14:21:06 +0800

    Admin section added a new season link

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -6,6 +6,9 @@
                 <x-nav-link href="/admin/venues/create" class="flex justify-center text-xl" :active="false">
                     Create a new Venue
                 </x-nav-link>
+                <x-nav-link href="/admin/season/create" class="flex justify-center text-xl" :active="false">
+                    Create a new Season
+                </x-nav-link>
                 <x-nav-link href="/admin/overview" class="flex justify-center text-xl" :active="false">
                     Overview of all administrators
                 </x-nav-link>
