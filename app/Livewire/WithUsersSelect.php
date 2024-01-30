4f294dada05b7758af31ba2117079ebbe3dcbc1d
Dimitri Mostrey <dmostrey@yahoo.com>
2024-01-30 14:24:54 +0800

    WithUsersSelect.php experimented with last player participation date

--- app/Livewire/WithUsersSelect.php
+++ app/Livewire/WithUsersSelect.php
@@ -23,6 +23,9 @@ trait WithUsersSelect
             ->orderBy('name')
             ->whereNotIn('id', [1]) //get rid of the administrator
             ->get(['id', 'name', 'last_game'])
+            /*->map(function (User $user) {
+                return ['id' => $user->id, 'name' => $user->name.' ('.$user->last_game->format('d-m-Y').')'];
+            })*/
             ->pluck('name', 'id')
             ->toArray();
     }
