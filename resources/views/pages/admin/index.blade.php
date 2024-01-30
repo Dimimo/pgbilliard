0e526369e68bad9737a98d2256e9b1575de26114
Dimitri Mostrey <dmostrey@yahoo.com>
2024-01-30 18:46:19 +0800

    Admin index.blade.php Calendar link added for current season

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -1,18 +1,30 @@
+<?php
+
+use function Livewire\Volt\uses;
+use App\Livewire\WithCurrentCycle;
+
+uses([WithCurrentCycle::class]);
+?>
 <x-layout>
     @volt
-        <section>
-            <x-title title="The Administration menu"/>
-            <div class="flex flex-col">
-                <x-nav-link href="/admin/venues/create" class="flex justify-center text-xl" :active="false">
-                    Create a new Venue
-                </x-nav-link>
-                <x-nav-link href="/admin/season/create" class="flex justify-center text-xl" :active="false">
-                    Create a new Season
-                </x-nav-link>
-                <x-nav-link href="/admin/overview" class="flex justify-center text-xl" :active="false">
-                    Overview of all administrators
+    <section>
+        <x-title title="The Administration menu"/>
+        <div class="flex flex-col">
+            <x-nav-link href="/admin/venues/create" class="flex justify-center text-xl" :active="false">
+                Create a new Venue
+            </x-nav-link>
+            <x-nav-link href="/admin/season/create" class="flex justify-center text-xl" :active="false">
+                Create a new Season
+            </x-nav-link>
+            <x-nav-link href="/admin/overview" class="flex justify-center text-xl" :active="false">
+                Overview of all administrators
+            </x-nav-link>
+            <div class="m-2 p-2 border border-blue-600 border-2 rounded">
+                <x-nav-link href="/admin/calendar/update/{{ $season->id }}" class="flex justify-center text-xl" :active="false">
+                    Update the current Calendar ({{ $cycle }})
                 </x-nav-link>
             </div>
-        </section>
+        </div>
+    </section>
     @endvolt
 </x-layout>
