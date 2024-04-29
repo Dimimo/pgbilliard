b96a7557eb4968ba8c6b7b56b8d8a6b5b87a593f
Dimitri Mostrey <dmostrey@yahoo.com>
2024-04-29 23:04:13 +0800

    Forum constants have been added

--- app/Constants.php
+++ app/Constants.php
@@ -12,6 +12,10 @@ class Constants
 
     public const PHONECHARS = 24;
 
+    public const FORUM_TITLE = 80;
+
+    public const FORUM_BODY = 1200;
+
     public const DATEFORMAT_START = 'Y-m-d 12:00:00';
 
     public const DATEFORMAT_END = 'Y-m-d 12:00:00';
