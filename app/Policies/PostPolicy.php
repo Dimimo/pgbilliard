d2233c48e926bdb49430990418a94a1d91f5cd25
Dimitri Mostrey <dmostrey@yahoo.com>
2024-05-01 16:11:03 +0800

    Post policy only admins can change locked posts

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -36,7 +36,7 @@ class PostPolicy
 
     public function update(User $user, Post $post): bool
     {
-        return $user->id === $post->user_id;
+        return $user->id === $post->user_id && ! $post->is_locked;
     }
 
     public function delete(User $user, Post $post): bool
