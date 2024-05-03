b95f4b20f4cd80852ae3247eae75b776dd3800b6
Dimitri Mostrey <dmostrey@yahoo.com>
2024-05-03 19:12:29 +0800

    with wire navigation

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -13,17 +13,17 @@ uses([WithCurrentCycle::class]);
     <section>
         <x-title title="The Administration menu"/>
         <div class="flex flex-col">
-            <x-nav-link href="{{ route('admin.venues.create') }}" class="flex justify-center text-xl" :active="false">
+            <x-nav-link href="{{ route('admin.venues.create') }}" class="flex justify-center mx-auto text-xl" :active="false" wire:navigate>
                 Create a new Venue
             </x-nav-link>
-            <x-nav-link href="{{ route('admin.season.create') }}/admin/season/create" class="flex justify-center text-xl" :active="false">
+            <x-nav-link href="{{ route('admin.seasons.create') }}/admin/season/create" class="flex justify-center mx-auto text-xl" :active="false" wire:navigate>
                 Create a new Season
             </x-nav-link>
-            <x-nav-link href="{{ route('admin.overview') }}" class="flex justify-center text-xl" :active="false">
+            <x-nav-link href="{{ route('admin.overview') }}" class="flex justify-center mx-auto text-xl" :active="false">
                 Overview of all administrators
             </x-nav-link>
-            <div class="m-2 p-2 border-blue-600 border-2 rounded">
-                <x-nav-link href="{{ route('admin.calendar.update', ['season' => $season]) }}" class="flex justify-center text-xl" :active="false">
+            <div class="flex justify-center m-2 p-2 border-blue-600 border-2 rounded">
+                <x-nav-link href="{{ route('admin.calendar.update', ['season' => $season]) }}" class="flex justify-center mx-auto text-xl" :active="false" wire:navigate>
                     Update the current Calendar ({{ $cycle }})
                 </x-nav-link>
             </div>
