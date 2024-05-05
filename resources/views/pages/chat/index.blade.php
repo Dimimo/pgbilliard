e0cf1ac61edd2b3f3aada598ff8f1d52fa07c0e1
Dimitri Mostrey <dmostrey@yahoo.com>
2024-05-05 22:58:16 +0800

    Guest login to chat has a more beautiful presentation

--- resources/views/pages/chat/index.blade.php
+++ resources/views/pages/chat/index.blade.php
@@ -14,9 +14,11 @@ name('chat.index');
         @can('create', \App\Models\Chat\ChatRoom::class)
             <livewire:chat.index/>
         @else
-            <div class="text-red-700 text-xl">
+            <div class="text-xl">
                 You don't have access to this page, to access the Chat,
-                simply <a href="{{ route('login') }}" wire:navigate>log in onto your account</a>.
+                simply <a class="text-blue-700 hover:underline" href="{{ route('login') }}" wire:navigate>log in onto your account</a>,
+                <a class="text-blue-700 hover:underline" href="{{ route('players.accounts') }}" wire:navigate>claim an existing account</a>
+                or <a class="text-blue-700 hover:underline" href="{{ route('register') }}" wire:navigate>create new account</a>.
             </div>
         @endcan
     </section>
