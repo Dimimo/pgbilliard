d528037eae40a049669dbd7e54db9b5630bb34b0
Dimitri Mostrey <dmostrey@yahoo.com>
2024-05-05 20:44:13 +0800

    Improved the admin links, added the Teams and Calendar view

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -13,19 +13,49 @@ uses([WithCurrentCycle::class]);
     <section>
         <x-title title="The Administration menu"/>
         <div class="flex flex-col">
-            <x-nav-link href="{{ route('admin.venues.create') }}" class="flex justify-center mx-auto text-xl" :active="false" wire:navigate>
+            <x-nav-link
+                href="{{ route('admin.venues.create') }}"
+                class="flex justify-center mx-auto text-xl" :active="false"
+                wire:navigate
+            >
                 Create a new Venue
             </x-nav-link>
-            <x-nav-link href="{{ route('admin.seasons.create') }}/admin/season/create" class="flex justify-center mx-auto text-xl" :active="false" wire:navigate>
+            <x-nav-link
+                href="{{ route('admin.seasons.create') }}"
+                class="flex justify-center mx-auto text-xl"
+                :active="false"
+                wire:navigate
+            >
                 Create a new Season
             </x-nav-link>
-            <x-nav-link href="{{ route('admin.overview') }}" class="flex justify-center mx-auto text-xl" :active="false">
-                Overview of all administrators
+            <x-nav-link
+                href="{{ route('admin.overview') }}"
+                class="flex justify-center mx-auto text-xl"
+                :active="false"
+            >
+                Overview of all Administrators
             </x-nav-link>
-            <div class="flex justify-center m-2 p-2 border-blue-600 border-2 rounded">
-                <x-nav-link href="{{ route('admin.calendar.update', ['season' => $season]) }}" class="flex justify-center mx-auto text-xl" :active="false" wire:navigate>
-                    Update the current Calendar ({{ $cycle }})
-                </x-nav-link>
+            <div class="m-2 p-2 w-auto border-blue-600 border-2 rounded-xl">
+                <div class="flex justify-center">
+                    <x-nav-link
+                        href="{{ route('admin.calendar.update', ['season' => $season]) }}"
+                        class="text-center mx-auto text-xl"
+                        :active="false"
+                        wire:navigate
+                    >
+                        Update the current Calendar ({{ $cycle }})
+                    </x-nav-link>
+                </div>
+                <div class="flex justify-center">
+                    <x-nav-link
+                        href="{{ route('admin.teams.create', ['season' => $season]) }}"
+                        class="text-center mx-auto text-xl"
+                        :active="false"
+                        wire:navigate
+                    >
+                        Update or create teams, their venue and captain, add or remove a BYE ({{ $cycle }})
+                    </x-nav-link>
+                </div>
             </div>
         </div>
     </section>
