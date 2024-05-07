dc4e15cfa6d95d2b502be964334867a8383baa99
Dimitri Mostrey <dmostrey@yahoo.com>
2024-05-08 00:03:05 +0800

    Layout got rid of the bg-gray-100

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased">
-<div class="min-h-screen mx-auto sm:px-4 bg-gray-100">
+<div class="min-h-screen mx-auto sm:px-4">
     @auth
         <livewire:layout.navigation/>
     @else
