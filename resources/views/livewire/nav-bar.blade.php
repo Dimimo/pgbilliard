c90a0d13d71093e46acd3306efec1c6543d4eba6
Dimitri Mostrey <dmostrey@yahoo.com>
2024-05-08 17:00:05 +0800

    Nav bar reordering with underline and top picture bg color and shadow

--- resources/views/livewire/nav-bar.blade.php
+++ resources/views/livewire/nav-bar.blade.php
@@ -4,7 +4,12 @@
     @endif
     <div class="flex flex-wrap mb-4">
         <div class="w-full align-center">
-            <img src="{{ secure_asset('images/title-small.png') }}" width="100%" alt="Pool League logo">
+            <img
+                src="{{ secure_asset('images/title-small.png') }}"
+                class="m-4 p-2 bg-gray-50 border-1 drop-shadow-lg rounded-xl"
+                width="100%"
+                alt="Pool League logo"
+            >
 
             <livewire:cycle-select/>
 
--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,4 +1,4 @@
-<div class="p-1 text-right z-10">
+<div class="p-1 mb-2 text-right z-10 border-b-2 border-gray-500">
     @auth
         <a href="{{ route('dashboard') }}"
            class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
@@ -7,9 +7,6 @@
             <a href="{{ route('admin.index') }}"
                class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Administration</a>
         @endif
-        <a href="{{ route('chat.index') }}"
-           class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-           wire:navigate>Chat</a>
     @else
         <a href="{{ route('login') }}"
            class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Log in</a>
@@ -22,9 +19,12 @@
            class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Claim
             existing account</a>
     @endauth
-        <a href="{{ route('scoresheet') }}"
-           class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-           wire:navigate>Scoresheet</a>
+    <a href="{{ route('scoresheet') }}"
+       class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+       wire:navigate>Scoresheet</a>
+    <a href="{{ route('chat.index') }}"
+       class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+       wire:navigate>Chat</a>
     <a href="{{ route('forum.posts.index') }}"
        class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
        wire:navigate>Forum</a>
