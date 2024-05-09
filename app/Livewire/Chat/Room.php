fe50b7ed4975b00759299fd516313e477c6971e8
Dimitri Mostrey <dmostrey@yahoo.com>
2024-05-09 18:06:57 +0800

    Improved chat room with invited users and how to administer them if you own the room

--- app/Livewire/Chat/Room.php
+++ app/Livewire/Chat/Room.php
@@ -3,12 +3,22 @@
 namespace App\Livewire\Chat;
 
 use App\Models\Chat\ChatRoom;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Room extends Component
 {
     public ChatRoom $room;
+
+    public Collection $list_users;
+
+    #[Validate(['nullable', 'string'])]
+    public ?string $search;
+
+    private array $ids;
 
     public function mount(ChatRoom $chatRoom): void
     {
@@ -17,10 +27,56 @@ class Room extends Component
             'owner',
             'messages.user' => fn ($q) => $q->select(['id', 'name']),
         ]);
+        $this->search = '';
+        $this->getListUsers();
     }
 
     public function render(): View
     {
         return view('livewire.chat.room');
+    }
+
+    public function updatedSearch(string $search): void
+    {
+        $this->search = $search;
+        $this->getListUsers();
+        $this->search = '';
+    }
+
+    public function toggleUser(int $user_id): void
+    {
+        $this->room->users()->toggle($user_id);
+        $this->room->refresh()->loadMissing([
+            'users' => fn ($q) => $q->select(['id', 'name']),
+            'owner',
+            'messages.user' => fn ($q) => $q->select(['id', 'name']),
+
+        ]);
+        $this->getListUsers();
+    }
+
+    public function deleteUser(int $user_id): void
+    {
+        //
+    }
+
+    private function getListUsers(): void
+    {
+        $this->getIds();
+        if ($this->search) {
+            $this->list_users = User::whereNotIn('id', $this->ids)
+                ->where('name', 'LIKE', "%$this->search%")
+                ->orderBy('name')
+                ->get(['id', 'name']);
+        } else {
+            $this->list_users = User::whereNotIn('id', $this->ids)
+                ->orderBy('name')
+                ->get(['id', 'name']);
+        }
+    }
+
+    private function getIds(): void
+    {
+        $this->ids = array_merge($this->room->users->pluck('id')->toArray(), [1]);
     }
 }
---
+++ resources/views/components/chat/users-search-dropdown.blade.php
@@ -0,0 +1,25 @@
+@props(['room', 'list_users'])
+
+<div>
+    <div class="relative font-[sans-serif] w-max">
+        <button type="button"
+                class="px-6 py-2 rounded text-white text-sm font-semibold border-none outline-none bg-blue-600 hover:bg-blue-700 active:bg-blue-600">
+            Invite people
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-3 fill-white inline ml-3" viewBox="0 0 24 24">
+                <path fill-rule="evenodd"
+                      d="M11.99997 18.1669a2.38 2.38 0 0 1-1.68266-.69733l-9.52-9.52a2.38 2.38 0 1 1 3.36532-3.36532l7.83734 7.83734 7.83734-7.83734a2.38 2.38 0 1 1 3.36532 3.36532l-9.52 9.52a2.38 2.38 0 0 1-1.68266.69734z"
+                      clip-rule="evenodd" data-original="#000000"/>
+            </svg>
+        </button>
+        <ul class='absolute shadow-lg bg-white py-2 px-2 z-[1000] min-w-full w-max rounded max-h-96 overflow-auto'>
+            <li class="mb-2">
+                <input
+                    placeholder="Text input"
+                    class="px-4 py-2.5 w-full rounded text-gray-500 text-sm font-semibold border-none outline-blue-600 bg-gray-50"
+                    wire:model.live.debounce="search"
+                />
+            </li>
+            <x-chat.user-search-item :room="$room" :users="$list_users"/>
+        </ul>
+    </div>
+</div>
