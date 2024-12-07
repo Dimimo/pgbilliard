c6e21f492b77c4a365be2344722b8da3e17914b5
Dimitri Mostrey <dmostrey@yahoo.com>
2024-12-07 12:05:07 +0800

    improved the posts table, moved to flexible svg files

--- resources/views/components/forum/posts-table.blade.php
+++ resources/views/components/forum/posts-table.blade.php
@@ -1,61 +1,71 @@
 <div id="top-posts" class="overflow-x-auto">
     <table class="min-w-full bg-white">
-        <thead class="bg-gray-800 whitespace-nowrap">
+        <thead class="whitespace-nowrap bg-gray-800">
         <tr>
             <th></th>
             <th class="px-6 py-3 text-left text-sm font-semibold text-white"></th>
             <th class="px-6 py-3 text-left text-sm font-semibold text-white">Started by</th>
             <th class="px-6 py-3 text-left text-sm font-semibold text-white">Last update</th>
             <th></th>
-            @if(Auth::check() && Auth::user()->isAdmin())
+            @if(!Auth::check() && Auth::user()->isAdmin())
                 <th class="px-6 py-3 text-left text-sm font-semibold text-white">Sticky?</th>
+            @else
+                <th class="px-6 py-3 text-left text-sm font-semibold text-white">Locked?</th>
             @endif
-            <th class="px-6 py-3 text-left text-sm font-semibold text-white">Locked?</th>
             <th class="px-6 py-3 text-left text-sm font-semibold text-white">Actions</th>
         </tr>
         </thead>
         <tbody class="whitespace-nowrap">
         @foreach($posts as $post)
-            <tr class="hover:bg-blue-50" wire:key="{{ $post->id }}">
-                <td class="px-6 py-3 text-sm">
+            <tr class="h-8 hover:bg-blue-50" wire:key="{{ $post->id }}">
+                <td>
                     @if ($post->visits->count() === 1)
-                        <img class="inline-block" src="{{ secure_asset('svg/envelop-open.svg') }}" alt="Post is read" width="22" height="22">
+                        <div class="ml-4" title="Has been opened">
+                            <x-svg.envelope-solid color="fill-green-700" size="5"/>
+                        </div>
                     @else
-                        <img class="inline-block" src="{{ secure_asset('svg/envelop-closed.svg') }}" alt="New post" width="22" height="22">
+                        <div class="ml-4" title="Not opened yet">
+                            <x-svg.envelope-open-text-solid color="fill-black" size="5"/>
+                        </div>
                     @endif
                 </td>
-                <td class="pl-6 py-3 @if($post->is_sticky)font-bold @endif">
-                    <a class="block" href="{{ route('forum.posts.show', ['post' => $post]) }}" wire:navigate>
+                <td @class(['font-bold' => $post->is_sticky])>
+                    <a class="ml-4 whitespace-nowrap" href="{{ route('forum.posts.show', ['post' => $post]) }}" wire:navigate>
                         {{ $post->title }}
                     </a>
                 </td>
-                <td class="px-6 py-3 text-sm">
-                    {{ $post->user->name }}
+                <td>
+                    <div class="ml-4 text-sm">
+                        {{ $post->user->name }}
+                    </div>
                 </td>
-                <td class="px-6 py-3 text-sm">
-                    {{ $post->updated_at->diffForHumans(['parts' => 2, 'join' => true, 'short' => true]) }}
+                <td>
+                    <div class="ml-4 text-sm">
+                        {{ $post->updated_at->diffForHumans(['parts' => 1, 'short' => false]) }}
+                    </div>
                 </td>
-                <td class="px-6 py-3 text-sm">
+                <td>
                     @if($post->comments_count > 0)
-                        <img class="mx-auto inline-block" src="{{ secure_asset('svg/comments.svg') }}" alt="Comments" width="22"
-                             height="22"> {{ $post->comments_count }}
+                        <x-svg.comments-solid color="fill-black" size="5"/> {{ $post->comments_count }}
                     @endif
                 </td>
-                @if(Auth::check() && Auth::user()->isAdmin())
-                    <td class="px-6 py-3 text-center">
-                        <label>
+                @if(!Auth::check() && Auth::user()->isAdmin())
+                    <td>
+                        <label class="flex justify-center">
                             <input type="checkbox" @if($post->is_sticky) checked @endif wire:click="toggle_sticky({{ $post->id }})"/>
                         </label>
                     </td>
-                    <td class="px-6 py-3 text-center">
-                        <label>
+                    <td>
+                        <label class="flex justify-center">
                             <input type="checkbox" @if($post->is_locked) checked @endif wire:click="toggle_locked({{ $post->id }})"/>
                         </label>
                     </td>
                 @else
-                    <td class="px-6 py-3 text-center">
+                    <td>
                         @if($post->is_locked)
-                            <img class="mx-auto inline-block" src="{{ secure_asset('svg/lock.svg') }}" alt="Post is locked" width="22" height="22">
+                            <div class="flex justify-center" title="Post is locked">
+                                <x-svg.lock-solid color="fill-gray-500" size="5"/>
+                            </div>
                         @endif
                     </td>
                 @endif
@@ -67,7 +77,7 @@
                     @endcan
                     @can('delete', $post)
                         <div
-                            class="inline-block px-1 cursor-pointer"
+                            class="inline-block cursor-pointer px-1"
                             wire:click="delete({{ $post->id }})"
                             wire:confirm="Are you sure you want to delete the post?"
                         >
