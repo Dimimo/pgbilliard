7c15951bb9d550afa6a593c1952272836c0b273f
Dimitri Mostrey <dmostrey@yahoo.com>
2024-12-09 15:07:26 +0800

    a language file for plurality

---
+++ resources/lang/en/plural.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'games' => '{0} <span class="text-blue-600">no games</span>|{1} <span class="text-red-600">one game</span>|[2,3] <span class="text-red-600">:count games</span>|[4] <span class="text-green-700">:count games</span>',
+];
