9a643955a30e226ddaef9dd075cf42e9128c163f
Dimitri Mostrey <dmostrey@yahoo.com>
2024-12-11 11:12:12 +0800

    navigation was not set for small screen when not logged in

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -271,6 +271,27 @@ $logout = function () {
                     </button>
                 </div>
             </div>
+            @else
+            <!-- Responsive Log in Options -->
+            <div class="border-t border-gray-200 pt-4 pb-1">
+                <div class="px-4">
+                    <div class="text-sm font-medium text-gray-800">Log In / Register</div>
+                </div>
+
+                <div class="mt-3 space-y-1">
+                    <x-forms.responsive-nav-link :href="route('login')" wire:navigate>
+                        {{ __('Log in') }}
+                    </x-forms.responsive-nav-link>
+
+                    <x-forms.responsive-nav-link :href="route('players.accounts')" wire:navigate>
+                        {{ __('Claim your account') }}
+                    </x-forms.responsive-nav-link>
+
+                    <x-forms.responsive-nav-link :href="route('register')" wire:navigate>
+                        {{ __('Register') }}
+                    </x-forms.responsive-nav-link>
+                </div>
+            </div>
         @endauth
     </div>
 </nav>
