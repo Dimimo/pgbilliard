d1429883a8468e1c633b4006c9fff20dc6c56902
Dimitri Mostrey <dmostrey@yahoo.com>
2024-12-13 09:33:36 +0800

    ably update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,24 +5,25 @@
  */
 
 import axios from "axios";
-//import Echo from "@ably/laravel-echo";
-//import * as Ably from "ably";
+import Echo from "@ably/laravel-echo";
+import * as Ably from "ably";
 
 window.axios = axios;
-/*window.Ably = Ably;
+window.Ably = Ably;
 window.Echo = new Echo({
     broadcaster: 'ably',
-});*/
+    authEndpoint: '/broadcasting/auth'
+});
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /* Ably */
-/*window.Echo.connector.ably.connection.on((stateChange) => {
+window.Echo.connector.ably.connection.on((stateChange) => {
     console.log("LOGGER:: Connection event :: ", stateChange);
     if (stateChange.current === 'disconnected' && stateChange.reason?.code === 40142) { // key/token status expired
         console.log("LOGGER:: Connection token expired https://help.ably.io/error/40142");
     }
-});*/
+});
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
