746a448da033036c5e9340530edafc5c830c6fd3
Dimitri Mostrey <dmostrey@yahoo.com>
2024-12-20 09:51:50 +0800

    compacted the file for better presentation

--- app/Constants.php
+++ app/Constants.php
@@ -5,26 +5,15 @@ namespace App;
 class Constants
 {
     public const VENUECHARS = 24;
-
     public const USERCHARS = 24;
-
     public const TEAMCHARS = 16;
-
     public const PHONECHARS = 24;
-
     public const FORUM_TITLE = 80;
-
     public const FORUM_BODY = 1200;
-
     public const COMMENT_BODY = 250;
-
     public const CHATROOM_TITLE = 32;
-
     public const CHATROOM_MESSAGE = 150;
-
     public const DATE_TITLE = 20;
-
     public const DATEFORMAT_START = 12;
-
     public const DATEFORMAT_END = 20;
 }
