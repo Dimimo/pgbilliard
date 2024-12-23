627b21035170105d1982a92e01101a36ceaeec2c
Dimitri Mostrey <dmostrey@yahoo.com>
2024-12-23 12:59:59 +0800

    the day score emails split up in an including body, also used for the admin email

---
+++ resources/views/mail/scores/_day-score-body.blade.php
@@ -0,0 +1,11 @@
+<x-mail::table>
+| Home Team | Visitors  |   Score   |
+|:---------:|:---------:|:---------:|
+@foreach($date->events as $event)
+| {{ $event->team_1->name }} | {{ $event->team_2->name }} | {{ $event->score1 }} - {{ $event->score2 }} |
+@endforeach
+</x-mail::table>
+
+<x-mail::button :url="route('scoresheet')">
+Scoresheet
+</x-mail::button>
--- resources/views/mail/scores/day-score.blade.php
+++ resources/views/mail/scores/day-score.blade.php
@@ -1,17 +1,7 @@
 <x-mail::message>
 # {{ $subject }}
 
-<x-mail::table>
-| Home Team | Visitors  |   Score   |
-|:---------:|:---------:|:---------:|
-@foreach($date->events as $event)
-| {{ $event->team_1->name }} | {{ $event->team_2->name }} | {{ $event->score1 }} - {{ $event->score2 }} |
-@endforeach
-</x-mail::table>
-
-<x-mail::button :url="route('scoresheet')">
-Scoresheet
-</x-mail::button>
+@include('mail.scores._day-score-body')
 
 Thanks,<br>
 {{ config('app.name') }}
