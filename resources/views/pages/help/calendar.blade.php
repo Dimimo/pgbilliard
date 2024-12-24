89120c0999b022bceaade7ae2c2f0cdc3229d6a3
Dimitri Mostrey <dmostrey@yahoo.com>
2024-12-24 18:56:16 +0800

    help files for the Calendar

---
+++ resources/views/pages/help/calendar.blade.php
@@ -0,0 +1,23 @@
+<?php
+
+use function Laravel\Folio\name;
+
+name('help.calendar');
+?>
+<x-layout>
+    @volt
+
+    <section>
+        <x-title>
+            <x-slot:title>The help pages</x-slot:title>
+            <x-slot:subtitle>
+                <x-svg.circle-question-solid color="fill-green-600" size="6"/>
+                Calendar, Schedule and Results
+            </x-slot:subtitle>
+        </x-title>
+
+        <x-help.calendar/>
+    </section>
+
+    @endvolt
+</x-layout>
