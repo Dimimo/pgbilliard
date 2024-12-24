6e1894076edef48cf703f92b8164862dbfcb2d21
Dimitri Mostrey <dmostrey@yahoo.com>
2024-12-24 21:29:55 +0800

    got rid of some extra spaces and commented out code

--- app/Livewire/WithUsersSelect.php
+++ app/Livewire/WithUsersSelect.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Carbon;
 trait WithUsersSelect
 {
     public array $users;
-
     public string $carbon_sub = '20 years';
 
     public function MountWithUsersSelect(): void
@@ -23,15 +22,13 @@ trait WithUsersSelect
             ->orderBy('name')
             ->whereNotIn('id', [1]) //get rid of the administrator
             ->get(['id', 'name', 'last_game'])
-            /*->map(function (User $user) {
-                return ['id' => $user->id, 'name' => $user->name.' ('.$user->last_game->format('d-m-Y').')'];
-            })*/
             ->pluck('name', 'id')
             ->toArray();
     }
 
     public function updatedWithUsersSelect($model, $value): void
     {
+        $this->validateOnly('user_id');
         if ($model === 'carbon_sub') {
             $this->carbon_sub = $value;
             $this->loadUsersList();
