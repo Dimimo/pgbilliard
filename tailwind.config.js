bf5c39107ef0980dee0bbc233bec00b9e7688b0f
Dimitri Mostrey <dmostrey@yahoo.com>
2024-12-27 21:30:55 +0800

    tailwind configuration adapted for the modal settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,21 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
+    safelist: [
+        'justify-end',
+        'max-w-xl',
+        'md:max-w-2xl',
+        {
+            pattern: /max-w-(sm|md|lg|xl|2xl|3xl|4xl|5xl|6xl|7xl)/,
+            variants: ['sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl']
+        }
+    ],
     theme: {
         extend: {
             fontFamily: {
