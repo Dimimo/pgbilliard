f90c12a9f30a24604cc3f157bf893ede66751705
Dimitri Mostrey <dmostrey@yahoo.com>
2024-12-27 22:02:56 +0800

    improved version of the help layout

--- resources/views/components/help/help-layout.blade.php
+++ resources/views/components/help/help-layout.blade.php
@@ -1,12 +1,15 @@
+@props(['help'])
 <div class="m-4">
-    <div class="mb-4 flex justify-between p-4 border-b-2 border-gray-300" title="close">
+    <div class="mb-4 flex justify-between p-4 border-b-2 border-gray-300">
         <div>
             <x-svg.circle-question-regular color="fill-green-600" size="6"/>
         </div>
-        <div class="text-green-700 text-lg">Scoreboard help</div>
-        <button wire:click="$dispatch('closeModal')" class="">
-            <x-svg.xmark-solid color="fill-gray-600" size="6" padding=""/>
-        </button>
+        <div class="text-green-700 text-lg">{{ $help }} help</div>
+        <div title="close">
+            <button wire:click="$dispatch('closeModal')">
+                <x-svg.xmark-solid color="fill-gray-600" size="6" padding=""/>
+            </button>
+        </div>
     </div>
 
     <div class="block">
