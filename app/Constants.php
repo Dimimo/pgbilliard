41d0880a4388525795fc238a7c702437238a4621
Dimitri Mostrey <dmostrey@yahoo.com>
2024-12-28 00:28:48 +0800

    chat room name can be no more than 12 chars

--- app/Constants.php
+++ app/Constants.php
@@ -11,7 +11,7 @@ class Constants
     public const FORUM_TITLE = 80;
     public const FORUM_BODY = 1200;
     public const COMMENT_BODY = 250;
-    public const CHATROOM_TITLE = 32;
+    public const CHATROOM_TITLE = 12;
     public const CHATROOM_MESSAGE = 150;
     public const DATE_TITLE = 20;
     public const DATEFORMAT_START = 12;
