0c3065b292005cd80c796a32b4cf3760f0216db9
Dimitri Mostrey <dmostrey@yahoo.com>
2024-12-29 06:51:36 +0800

    forgot password page improved

--- resources/views/livewire/pages/auth/forgot-password.blade.php
+++ resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -11,15 +11,13 @@ state(['email' => '']);
 
 rules(['email' => ['required', 'string', 'email']]);
 
-$sendPasswordResetLink = function ()
-{
+$sendPasswordResetLink = function () {
     $this->validate();
     // We will send the password reset link to this user. Once we have attempted
     // to send the link, we will examine the response then see the message we
     // need to show to the user. Finally, we'll send out a proper response.
     $status = Password::sendResetLink($this->only('email'));
-    if ($status != Password::RESET_LINK_SENT)
-    {
+    if ($status != Password::RESET_LINK_SENT) {
         $this->addError('email', __($status));
 
         return;
@@ -29,7 +27,7 @@ $sendPasswordResetLink = function ()
 };
 ?>
 
-<div class="mx-96">
+<div class="flex flex-col justify-center">
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')"/>
@@ -38,18 +36,18 @@ $sendPasswordResetLink = function ()
         <!-- Email Address -->
         <div>
             <x-forms.input-label for="email" :value="__('Email')"/>
-            <x-forms.text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus/>
+            <x-forms.text-input wire:model="email" id="email" class="mt-1 block w-full" type="email" name="email" required autofocus/>
             <x-forms.input-error :messages="$errors->get('email')" class="mt-2"/>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="mt-4 flex items-center justify-end">
             <x-forms.primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Email the password reset link') }}
             </x-forms.primary-button>
         </div>
     </form>
 
-    <div class="my-8 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div class="mx-auto my-8 w-96 text-sm text-gray-600">
+        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to create a new password.') }}
     </div>
 </div>
