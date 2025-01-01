4b2bfc1b5cc99a416435012b5dc627df0bd0b58f
Dimitri Mostrey <dmostrey@yahoo.com>
2025-01-01 19:05:57 +0800

    if the event is confirmed, show the scores on a subtitle

--- resources/views/pages/schedule/event/[Event].blade.php
+++ resources/views/pages/schedule/event/[Event].blade.php
@@ -19,7 +19,14 @@ name('schedule.event');
                 The schedule of <span class="font-bold">{{ $event->team_1->name }} - {{ $event->team_2->name }}</span>
             </x-slot:title>
             <x-slot:subtitle>
-                on the {{ $event->date->date->format('jS \o\f M Y') }}
+                <div>on the {{ $event->date->date->format('jS \o\f M Y') }}</div>
+                @if ($event->confirmed)
+                    <div class="mt-4">
+                        <span @class(['text-green-700' => $event->score1 > 7])>{{ $event->team_1->name }} {{ $event->score1 }}</span>
+                        <x-svg.minus-solid color="fill-gray-600" size="3" padding="mx-2"/>
+                        <span @class(['text-green-700' => $event->score2 > 7])>{{ $event->score2 }} {{ $event->team_2->name }}</span>
+                    </div>
+                @endif
             </x-slot:subtitle>
         </x-title>
         <livewire:date.schedule :event="$event"/>
