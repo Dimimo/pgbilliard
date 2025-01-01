f1efdc342253246bee8d7dedc9826d30dacde5fe
Dimitri Mostrey <dmostrey@yahoo.com>
2025-01-01 18:50:10 +0800

    pluralisation, dropped the word 'yet'

--- lang/en/plural.php
+++ lang/en/plural.php
@@ -2,5 +2,5 @@
 
 return [
     'games' => '{0} <span class="text-blue-600">no games</span>|{1} <span class="text-red-600">one game</span>|[2,3] <span class="text-red-600">:count games</span>|[4] <span class="text-green-700">:count games</span>',
-    'day-games' => '{0} no day schedule started yet|[2,*] day schedule available',
+    'day-games' => '{0} no day schedule started|[2,*] day schedule available',
 ];
