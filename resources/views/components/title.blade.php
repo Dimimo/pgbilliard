89ccd50e03030118e71e4d1d29a110e1a1d6e366
Dimitri Mostrey <dmostrey@yahoo.com>
2025-01-02 15:41:50 +0800

    changed the background color of the title and subtitle, to a green to transparent gradient

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,21 +1,21 @@
 @props(['title', 'subtitle' => null, 'help' => null])
-<div class="grid grid-rows-auto gap-y-2 py-3 px-6 mb-2 md:mb-4 bg-blue-100 border-2 border-blue-300 rounded-lg" {{ $attributes }}>
+<div class="grid grid-rows-auto gap-y-2 py-3 px-6 mb-2 md:mb-4 bg-gradient-to-b from-green-300 rounded-lg" {{ $attributes }}>
     @if($help)
         <div class="flex flex-row">
             <div class="flex-none"></div>
             <div class="grow">
-                <div class="text-center text-4xl text-blue-900">{!! $title !!}</div>
+                <div class="text-center text-4xl text-green-900">{!! $title !!}</div>
             </div>
             <div class="flex-none text-right" title="Get help for the {{ $help }}">
                 <button wire:click="$dispatch('openModal', { component: 'help.{{ $help }}' })">
-                    <x-svg.circle-question-regular color="fill-green-600" size="6"/>
+                    <x-svg.circle-question-regular color="fill-green-700" size="6"/>
                 </button>
             </div>
         </div>
     @else
-        <div class="text-center text-4xl text-blue-900">{!! $title !!}</div>
+        <div class="text-center text-4xl text-green-900">{!! $title !!}</div>
     @endif
     @if(isset($subtitle))
-        <div class="text-center text-xl text-cyan-800">{{ $subtitle }}</div>
+        <div class="text-center text-xl text-teal-700">{{ $subtitle }}</div>
     @endif
 </div>
