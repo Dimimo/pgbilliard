7fa3af6819efb690a1c8afc046902c0fb1f1b28d
Dimitri Mostrey <dmostrey@yahoo.com>
2025-01-04 13:11:48 +0800

    added 'teams' to plural

--- lang/en/plural.php
+++ lang/en/plural.php
@@ -2,5 +2,6 @@
 
 return [
     'games' => '{0} <span class="text-blue-600">no games</span>|{1} <span class="text-red-600">one game</span>|[2,3] <span class="text-red-600">:count games</span>|[4] <span class="text-green-700">:count games</span>',
-    'day-games' => '{0} no day schedule started|[2,*] day schedule available',
+    'day-games' => '{0} no day schedule started|{1} one day schedule available|[2,*] :count day schedules available',
+    'teams' => '{0} are no teams|{1} is one team|[2,*] are :count teams',
 ];
