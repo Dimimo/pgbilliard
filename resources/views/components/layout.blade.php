339fce26c4bd7b73eb5327358ca73aff739f538a
Dimitri Mostrey <dmostrey@yahoo.com>
2025-01-23 11:15:52 +0800

    some small changes on the layout file

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,10 +1,9 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3.0, user-scalable=yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.25, maximum-scale=3.0, user-scalable=yes">
     <meta name="author" content="Dimitri Mostrey (www.pgbilliard.com)">
     <meta name="copyright" content="&copy; &reg; {{ date("Y") }} Puerto Galera Pool League">
     <meta name="description" content="Puerto Galera Pool League">
