f7f17a7b579a9832738f7faeb40842428993b3a4
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-08 11:45:40 +0800

    the resources ably js file

---
+++ resources/js/ably.js
@@ -0,0 +1,16 @@
+import Echo from '@ably/laravel-echo';
+import * as Ably from 'ably';
+
+window.Ably = Ably;
+window.Echo = new Echo({
+    broadcaster: 'ably',
+});
+
+window.Echo.connector.ably.connection.on(stateChange => {
+    if (stateChange.current === 'connected') {
+        console.log("LOGGER:: Connection event :: ", stateChange);
+        if (stateChange.current === 'disconnected' && stateChange.reason?.code === 40142) { // key/token status expired
+            console.log("LOGGER:: Connection token expired https://help.ably.io/error/40142");
+        }
+    }
+});
