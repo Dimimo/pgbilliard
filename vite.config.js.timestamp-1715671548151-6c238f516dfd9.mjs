d661c6d15f70f97987de289b9995bdb3823aadb2
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-12 13:03:42 +0800

    vite config.js timestamped file

--- vite.config.js.timestamp-1715671548151-6c238f516dfd9.mjs
+++ vite.config.js.timestamp-1715671548151-6c238f516dfd9.mjs
@@ -7,7 +7,7 @@ var cert_crt = `.config/herd/config/valet/Certificates/${host}.crt`;
 var vite_config_default = defineConfig({
   plugins: [
     laravel({
-      input: ["resources/css/app.css", "resources/js/app.js"],
+      input: ["resources/css/app.css", "resources/js/app.js", "resources/js/ably.js"],
       refresh: [
         "resources/views/**",
         "app/Livewire/**",
