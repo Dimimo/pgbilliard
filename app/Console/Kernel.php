72a1373e36c103165596eb280253c6d26b20407c
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-12 15:53:24 +0800

    test on running the redis server every 5 minutes with the help of the console kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('day-scores')->dailyAt('12:00');
+        $schedule->command('redis-server >> /dev/null 2>&1')->everyFiveMinutes()->runInBackground();
         $schedule->job(new PlayDayReminder())->dailyAt('12:00');
 
         if (str_contains(shell_exec('ps xa'), 'tries=2') === false) {
