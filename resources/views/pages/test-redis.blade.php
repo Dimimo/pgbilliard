12c1446d8ea98550e655f866ae554323800e1157
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-12 14:39:58 +0800

    a redis test with Redis\Client

---
+++ resources/views/pages/test-redis.blade.php
@@ -0,0 +1,30 @@
+<?php
+
+use function Livewire\Volt\state;
+use Predis\Client;
+
+state(['result' => function() {
+    $client = new Predis\Client([
+        'host' => 'redis-19999.c295.ap-southeast-1-1.ec2.redns.redis-cloud.com',
+        'port' => 19999,
+        'database' => 0,
+        'username' => 'default',
+        'password'=> 'ur3kMiSuCpTY9sO4rWUSZCKARt90laP0',
+    ]);
+
+    $client->set('foo', 'bar');
+    return $client->get('foo');
+}])
+
+?>
+
+<x-layout>
+    @volt
+    <section>
+        <p>result is : {{ $result }}</p>
+
+    </section>
+    @endvolt
+</x-layout>
+
+
