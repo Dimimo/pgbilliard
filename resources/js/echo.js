c8db35e0827ac9d2d9533eec24eff14bd0126fc4
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-14 12:27:23 +0800

    echo.js updated according to the recommendations of laravel.com, but for now, the js file is not used, only ably.js is

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -4,12 +4,14 @@ import Pusher from "pusher-js";
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: "reverb",
+    broadcaster: "pusher",
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
-    enabledTransports: ["ws", "wss"],
+    wsHost: 'realtime-pusher.ably.io', //import.meta.env.VITE_REVERB_HOST,
+    wsPort: 443, //import.meta.env.VITE_REVERB_PORT ?? 80,
+    disableStats: true,
+    encrypted: true,
+    //wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    //forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
+    //enabledTransports: ["ws", "wss"],
     //cluster: "Redis",
 });
