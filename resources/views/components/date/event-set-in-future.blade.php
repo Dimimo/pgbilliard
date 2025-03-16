2f58177595eb85a75f0eb2297e3ade93d5c2d33b
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-17 03:55:38 +0800

    slightly changed the presentation, as to when the time window opens (Available in ...)

--- resources/views/components/date/event-set-in-future.blade.php
+++ resources/views/components/date/event-set-in-future.blade.php
@@ -1,5 +1,5 @@
 @props(['event'])
 
 <div class="text-sm text-gray-600">
-    Starts in {{ $event->date->date->appTimezone()->midDay()->addHours(2)->diffForHumans(['short' => false, 'parts' => 2, 'join' => true]) }}
+    {{__('Available in')}} {{ $event->date->date->appTimezone()->midDay()->diffForHumans(['short' => false, 'parts' => 2, 'join' => true]) }}
 </div>
