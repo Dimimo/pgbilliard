78bdc0c613c166d9ad08410029f22cc2f62373fd
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-17 02:56:57 +0800

    reword the plural choices of day-games as the number finished

--- lang/en/plural.php
+++ lang/en/plural.php
@@ -2,6 +2,6 @@
 
 return [
     'games' => '{0} <span class="text-blue-600">no games</span>|{1} <span class="text-red-600">one game</span>|[2,3] <span class="text-red-600">:count games</span>|[4] <span class="text-green-700">:count games</span>',
-    'day-games' => '{0} no day games available|{1} one game started|[2,*] :count games',
+    'day-games' => '{0} no games yet|{1} one game finished|[2,*] :count games finished',
     'teams' => '{0} are no teams|{1} is one team|[2,*] are :count teams',
 ];
