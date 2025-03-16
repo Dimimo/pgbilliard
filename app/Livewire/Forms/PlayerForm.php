fc0fb5a3fe1ea95cf2ff4ecff44fc86ae54a8c7c
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-16 22:37:18 +0800

    the player can be active is added to the PlayerForm

--- app/Livewire/Forms/PlayerForm.php
+++ app/Livewire/Forms/PlayerForm.php
@@ -6,19 +6,24 @@ use App\Models\Player;
 use App\Models\User;
 use Auth;
 use Illuminate\Validation\Rule;
+use Livewire\Attributes\Validate;
 use Livewire\Form;
 
 class PlayerForm extends Form
 {
     public Player $player;
     public bool $captain = false;
+    public bool $active = true;
+    #[Validate]
     public ?int $user_id;
+    #[Validate]
     public ?int $team_id;
 
     public function rules(): array
     {
         return [
             'captain' => 'bool',
+            'active' => 'bool',
             'user_id' => Rule::unique(User::class, 'id')->ignore(Auth::user()),
             'team_id' => [
                 'required',
@@ -40,6 +45,7 @@ class PlayerForm extends Form
     {
         $this->player = $player;
         $this->captain = $player->captain;
+        $this->active = $player->active;
         $this->user_id = $player->user_id;
         $this->team_id = $player->team_id;
     }
@@ -47,7 +53,7 @@ class PlayerForm extends Form
     public function store(): void
     {
         $this->validate();
-        Player::create($this->only(['captain', 'user_id', 'team_id']));
+        Player::create($this->only(['captain', 'active', 'user_id', 'team_id']));
     }
 
     public function update(): void
