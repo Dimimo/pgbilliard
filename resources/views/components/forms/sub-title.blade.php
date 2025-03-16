e63ab2e68dbeca59b8c564b19fd2f019b34d93a0
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-17 03:17:24 +0800

    better representation of the sub-title

--- resources/views/components/forms/sub-title.blade.php
+++ resources/views/components/forms/sub-title.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <div class="border-2 border-gray-500 bg-gray-100/25 my-4 mb-5">
-        <div class="border-b-2 border-gray-500 bg-gray-100 py-2 text-center text-3xl">
+    <div class="my-4 mb-5 rounded-lg border-2 border-gray-500 bg-gray-100/25">
+        <div class="rounded-t-lg border-b-2 border-gray-500 bg-gray-100 py-2 text-center text-3xl">
             {!! $title !!}
         </div>
         {{ $slot }}
