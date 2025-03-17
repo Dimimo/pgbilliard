17b3028e97986b5cc26af51c0881a8127377095d
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-17 22:02:27 +0800

    runs the backup clean and run command every night at 1am

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('day-scores')->dailyAt('12:00');
         $schedule->command('queue:start-redis-server')->everyFiveMinutes()->runInBackground();
         $schedule->job(new PlayDayReminder())->dailyAt('12:00');
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('01:15');
 
         if (str_contains(shell_exec('ps xa'), 'tries=2') === false) {
             //https://www.tecmint.com/run-linux-command-process-in-background-detach-process/
