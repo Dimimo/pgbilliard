5f75a3a8a022ec555c3b4daf28bb07c0fc12dbae
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-23 11:38:08 +0800

    Carbon diffForHumans shorter version (mobile friendly)

--- resources/views/components/date/event-set-in-future.blade.php
+++ resources/views/components/date/event-set-in-future.blade.php
@@ -1,5 +1,5 @@
-@props(['event'])
+@props(['event']) {{-- other options: 'short' => false, 'parts' => 2, 'join' => false, --}}
 
 <div class="text-sm text-gray-600">
-    {{__('Available in')}} {{ $event->date->date->appTimezone()->midDay()->addHour()->diffForHumans(['short' => false, 'parts' => 2, 'join' => true]) }}
+    {{__('Available in')}} {{ $event->date->date->appTimezone()->midDay()->longAbsoluteDiffForHumans(['syntax' => Carbon\CarbonInterface::DIFF_ABSOLUTE]) }}
 </div>
