270f8882a299ae555a3bf460217643ccae0cf2e9
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-24 15:09:52 +0800

    changed the class variable from post_form to simply form for consistency

--- app/Livewire/Forum/Posts/Create.php
+++ app/Livewire/Forum/Posts/Create.php
@@ -9,11 +9,11 @@ use Livewire\Component;
 
 class Create extends Component
 {
-    public PostForm $post_form;
+    public PostForm $form;
 
     public function mount(Post $post): void
     {
-        $this->post_form->setPost($post);
+        $this->form->setPost($post);
     }
 
     public function render(): View
@@ -23,28 +23,28 @@ class Create extends Component
 
     public function create(): void
     {
-        $this->authorize('create', $this->post_form->post);
-        $this->post_form->store();
+        $this->authorize('create', $this->form->post);
+        $this->form->store();
         $this->dispatch('post-saved');
         session()->flash('status', 'Your post is successfully created.');
-        $this->redirect(route('forum.posts.show', ['post' => $this->post_form->post]), navigate: true);
+        $this->redirect(route('forum.posts.index'));
     }
 
     public function update(): void
     {
-        $this->authorize('update', $this->post_form->post);
-        $this->post_form->update();
+        $this->authorize('update', $this->form->post);
+        $this->form->update();
         $this->dispatch('post-updated');
         session()->flash('status', 'Your post is successfully updated.');
-        $this->redirect(route('forum.posts.show', ['post' => $this->post_form->post]), navigate: true);
+        $this->redirect(route('forum.posts.index'));
     }
 
     public function delete(): void
     {
-        $this->authorize('delete', $this->post_form->post);
-        $this->post_form->delete();
+        $this->authorize('delete', $this->form->post);
+        $this->form->delete();
         $this->dispatch('post-deleted');
         session()->flash('status', 'The post is successfully deleted.');
-        $this->redirect(route('forum.posts.index'), navigate: true);
+        $this->redirect(route('forum.posts.index'));
     }
 }
--- resources/views/livewire/forum/posts/create.blade.php
+++ resources/views/livewire/forum/posts/create.blade.php
@@ -1,34 +1,34 @@
 <div>
     <div class="border-2 border-green-500 bg-green-100/25 p-6">
-        <form wire:submit="{{ $post_form->post->id ? "update" : "create" }}">
+        <form wire:submit="{{ $form->post->id ? "update" : "create" }}">
             <div class="grid justify-items-center">
-                <x-forms.input-label for="post_form.title">
+                <x-forms.input-label for="form.title">
                     Title
                 </x-forms.input-label>
                 <div>
-                    @error('post_form.title') <span class="text-red-700">{{ $message }}</span> @enderror
+                    @error('form.title') <span class="text-red-700">{{ $message }}</span> @enderror
                 </div>
                 <x-forms.text-input
-                    id="post_form.title"
+                    id="form.title"
                     class="w-full md:w-3/4"
                     type="text"
-                    wire:model="post_form.title"
+                    wire:model="form.title"
                     :maxlength="\App\Constants::FORUM_TITLE"
                 />
             </div>
 
             <div class="mt-6 grid justify-items-center">
-                <x-forms.input-label for="post_form.body">
+                <x-forms.input-label for="form.body">
                     Your message
                 </x-forms.input-label>
                 <div>
-                    @error('post_form.body') <span class="text-red-700">{{ $message }}</span> @enderror
+                    @error('form.body') <span class="text-red-700">{{ $message }}</span> @enderror
                 </div>
-                {{--<x-forms.text-area id="post_form.body" class="w-full md:w-3/4" cols="30" rows="5" wire:model="post_form.body"/>--}}
+                {{--<x-forms.text-area id="form.body" class="w-full md:w-3/4" cols="30" rows="5" wire:model="form.body"/>--}}
                 <div class="w-full">
                     <x-forms.textarea-constrained
-                        for="post_form.body"
-                        :value="$post_form->body"
+                        for="form.body"
+                        :value="$form->body"
                         :limit="\App\Constants::FORUM_BODY"
                     />
                 </div>
@@ -36,10 +36,10 @@
             </div>
 
             @if(session('is_admin'))
-                <x-forms.checkbox for="post_form.sticky" :checked="$post_form->is_sticky" wire:model="post_form.is_sticky">
+                <x-forms.checkbox for="form.sticky" :checked="$form->is_sticky" wire:model="form.is_sticky">
                     Sticky? <span class="text-sm">(pushed to the top for important messages)</span>
                 </x-forms.checkbox>
-                <x-forms.checkbox for="post_form.locked" :checked="$post_form->is_locked" wire:model="post_form.is_locked">
+                <x-forms.checkbox for="form.locked" :checked="$form->is_locked" wire:model="form.is_locked">
                     Locked? <span class="text-sm">(can only be changed by administrators)</span>
                 </x-forms.checkbox>
             @endif
@@ -47,7 +47,7 @@
             <div class="my-4 grid grid-cols-4 gap-2">
                 <div class="col-start-2 text-center">
                     <x-forms.primary-button>
-                        {{ $post_form->post->id ? "Update post" : "Create post" }}
+                        {{ $form->post->id ? "Update post" : "Create post" }}
                     </x-forms.primary-button>
                 </div>
                 <x-forms.spinner target="create, update, delete"/>
