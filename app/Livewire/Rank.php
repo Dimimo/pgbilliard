5a3e30027d739453bce9a0e1c538e0fe6b790039
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-27 14:29:52 +0800

    rank updated, should refresh (only for admins), still a WIP

--- app/Livewire/Rank.php
+++ app/Livewire/Rank.php
@@ -2,23 +2,39 @@
 
 namespace App\Livewire;
 
+use App\Jobs\UpdateRanks;
 use App\Models\Season;
 use Illuminate\Support\Collection;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Rank extends Component
 {
     public Collection $results;
+    public Season $season;
     public int $rank = 1;
 
     public function mount(): void
     {
-        $season = Season::where('cycle', session('cycle'))->first();
-        $this->results = $season->ranks()->orderByDesc('percentage')->get();
+        $this->season = Season::where('cycle', session('cycle'))->first();
+        $this->results = $this->season->ranks()->orderByDesc('percentage')->get();
     }
 
     public function render(): \Illuminate\View\View
     {
         return view('livewire.rank');
+    }
+
+    public function requestUpdate(): void
+    {
+        UpdateRanks::dispatchSync($this->season);
+        $this->dispatch('updated');
+    }
+
+    #[On('echo:refresh-requested,RefreshRequested')]
+    public function refreshRequested(): void
+    {
+        $this->results = $this->season->ranks()->orderByDesc('percentage')->get();
+        $this->dispatch('refresh-requested');
     }
 }
--- resources/views/livewire/rank.blade.php
+++ resources/views/livewire/rank.blade.php
@@ -1,4 +1,18 @@
 <div>
+    @if(session('is_admin'))
+        <div class="my-8 flex flex-row items-center space-x-2">
+            <button
+                type="button"
+                class="rounded-lg border border-indigo-700 bg-indigo-100 px-4 py-2"
+                wire:click="requestUpdate"
+            >
+                Request update
+            </button>
+            <div>
+                <x-forms.action-message on="updated">Updated!</x-forms.action-message>
+            </div>
+        </div>
+    @endif
     <ul>
         @foreach($results->take(30) as $result)
             <li>
@@ -10,4 +24,16 @@
             </li>
         @endforeach
     </ul>
+
+    @script
+    <script>
+        let echoPublicChannel = window.Echo.channel('refresh-requested');
+        let ablyPublicChannelName = echoPublicChannel.name;
+        console.log(ablyPublicChannelName);
+        $wire.on('refresh-requested', () => {
+            console.log('refresh requested');
+            $wire.$commit();
+        });
+    </script>
+    @endscript
 </div>
