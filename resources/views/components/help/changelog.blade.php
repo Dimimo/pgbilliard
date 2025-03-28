d758a67de52fbab9e8d954201bbab27a0bcc6b90
Dimitri Mostrey <dmostrey@yahoo.com>
2025-03-28 15:46:04 +0800

    changelog added the individual ranking feature

--- resources/views/components/help/changelog.blade.php
+++ resources/views/components/help/changelog.blade.php
@@ -44,7 +44,8 @@
         <li>
             Extra check of the 15th game (last double) added. If the 4 players are not selected, no checkboxes appear.
         </li>
-        <li>The last updated score is highlighted in color. On the Scoreboard, the Calendar and daily overview.<Oli>
+        <li>The last updated score is highlighted in color. On the Scoreboard, the Calendar and daily overview.<li>
+        <li>The individual ranking is now available <span class="font-bold">but still in beta!</span></li>
     </ul>
 </div>
 <div class="mt-4">
