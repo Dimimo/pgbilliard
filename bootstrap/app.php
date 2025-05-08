182980ce0f3cf0fe754d1283912019f51e62c1d6
Dimitri Mostrey <dmostrey@yahoo.com>
2025-05-08 18:40:41 +0800

    report an error to the admin email

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,13 +1,14 @@
 <?php
 
+use App\Mail\ExceptionMail;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -21,5 +22,15 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->report(function (\Exception $e) {
+            try {
+                $title = $e->getMessage() . ' (' . \URL::full() . ')';
+                $message = $title . "\n\n" . $e;
+                if (\App::environment() === 'production') {
+                    \Mail::to('admin@pgbilliard.com')->send(new ExceptionMail($title, nl2br($message)));
+                }
+            } catch (Exception $e) {
+                \Log::error($e->getMessage());
+            }
+        });
     })->create();
