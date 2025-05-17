f22af5830d60d6997dbf021c622e5eb6797f7003
Dimitri Mostrey <dmostrey@yahoo.com>
2025-05-17 13:04:04 +0800

    created the participation blade component, also for use in the individual player results overview

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -53,57 +53,9 @@
                 @endforeach
             </div>
         @endif
-        <div class="rounded-lg bg-indigo-50 p-4">
-            @if ($team)
-                <div class="mb-4 text-lg">{{__('Your participation')}}:</div>
-                <div class="mb-4">
-                    <div>
-                        {{__('You play for')}} <a
-                            href="{{ route('teams.show', ['team' => $team]) }}"
-                            class="inline-block text-blue-800 link"
-                            wire:navigate
-                        >
-                            {{ $team->name }}
-                        </a>
-                    </div>
-                    <div class="my-4">
-                        {{__('Your current')}} <a
-                            href="{{ route('rank') }}"
-                            class="inline-block text-blue-800 link"
-                            wire:navigate
-                        >{{__('individual rank is')}}</a>
-                        <span class="m-1 rounded-full border border-green-500 bg-green-100 px-2 py-1 font-bold">
-                            {{ $rank }}
-                        </span>
-                    </div>
-                    @if ($player->captain)
-                        <div>
-                            {{__('You are the Captain, you can')}} <a
-                                href="{{ route('teams.edit', ['team' => $team]) }}"
-                                class="inline-block text-blue-800 link"
-                                wire:navigate
-                            >
-                            {{__('manage your team here')}}
-                            </a>
-                        </div>
-                    @endif
-                </div>
-                <div class="text-lg">{{__('Your team members are')}}:</div>
-                <ul class="list-inside list-disc">
-                    @foreach($team->players->sortBy('name')->sortByDesc('captain') as $member)
-                        <li>{{ $member->user->name }} {{ $member->captain ? 'is captain' : '' }}</li>
-                    @endforeach
-                </ul>
 
-            @else
-                <div class="mb-4">
-                    {{__("You don't play for a team. If you think this is an error")}}
-                    <a href="{{ route('teams.index') }}" class="inline-block text-blue-800 link" wire:navigate>
-                        {{__('contact your captain or bar owner')}}
-                    </a>
-                </div>
-            @endif
-        </div>
+        <x-player.participation :team="$team" :player="$player" :rank="$rank"/>
+
     </div>
 
     <div class="mb-4 rounded-lg border-2 border-gray-500 p-2">
