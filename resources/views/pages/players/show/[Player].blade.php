e8f6e9d017e89c59e42f735741c80664b92b8a73
Dimitri Mostrey <dmostrey@yahoo.com>
2025-05-17 13:04:24 +0800

    the folio file for the player details

--- resources/views/pages/players/show/[Player].blade.php
+++ resources/views/pages/players/show/[Player].blade.php
@@ -1,3 +1,19 @@
-<div>
-    {{ dd($player) }}
-</div>
+<?php
+use function Laravel\Folio\name;
+use function Livewire\Volt\state;
+
+name('player.show');
+state('player');
+?>
+
+<x-layout title="Player details of {{ $player->name }}">
+    @volt
+    <section>
+        <x-title
+            title="Personal profile and results of {{ $player->name }}"
+            subtitle="Season {{ session('cycle') }}"
+        />
+        <livewire:players.details :player="$player"/>
+    </section>
+    @endvolt
+</x-layout>
