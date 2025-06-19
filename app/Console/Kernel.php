902f9dbcae49a8249f6d5746ed947e32d5fd26b4
Dimitri Mostrey <dmostrey@yahoo.com>
2025-06-19 14:04:50 +0800

    schedule moved to console routes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,24 +8,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
-     * Define the application's command schedule.
-     */
-    protected function schedule(Schedule $schedule): void
-    {
-        // $schedule->command('queue:work --tries=2 --max-time=900 --queue=high,default')->runInBackground();
-        $schedule->command('pool:day-scores')->dailyAt('12:00');
-        $schedule->command('pool:day-reminder')->dailyAt('12:00');
-        $schedule->command('backup:clean')->daily()->at('01:00');
-        $schedule->command('backup:run')->daily()->at('01:15');
-        // $schedule->command('queue:start-redis-server')->everyFiveMinutes()->runInBackground();
-
-        /*if (str_contains(shell_exec('ps xa'), 'tries=2') === false) {
-            //https://www.tecmint.com/run-linux-command-process-in-background-detach-process/
-            $schedule->command('queue:work --tries=2 --max-time=930 --queue=high,default')->runInBackground();
-        }*/
-    }
-
-    /**
      * Register the commands for the application.
      */
     protected function commands(): void
--- routes/console.php
+++ routes/console.php
@@ -2,18 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::dailyAt('12:00')
+    ->group(function () {
+        Schedule::command('pool:day-scores');
+        Schedule::command('pool:day-reminder');
+    });
+
+Schedule::dailyAt('01:00')
+    ->command('backup:run')
+    ->after(function () {
+        Schedule::command('backup:clean');
+    });
