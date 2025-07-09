6b18ca2cfb84d91411e261bcb35d1eef08409e2e
Dimitri Mostrey <dmostrey@yahoo.com>
2025-07-09 13:19:05 +0800

    Added an active player filter, it also listed players on hold

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 
 /**
@@ -113,6 +114,11 @@ class Team extends Model
     public function hasGames(): bool
     {
         return $this->team_1()->count() || $this->team_2()->count();
+    }
+
+    public function activePlayers(): Collection
+    {
+        return $this->players->filter(fn ($player) => $player->active === true);
     }
 
     /**************************************
