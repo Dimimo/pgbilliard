cc09be2e6ab1b2a0f39a0794c5f4388b459c80f3
Dimitri Mostrey <dmostrey@yahoo.com>
2025-07-23 15:20:56 +0800

    added some tables that don't need to be dumped in the daily backup, also the 'storage' folder is completely omitted

--- config/backup.php
+++ config/backup.php
@@ -26,6 +26,7 @@ return [
                 'exclude' => [
                     base_path('vendor'),
                     base_path('node_modules'),
+                    base_path('storage'),
                 ],
 
                 /*
@@ -36,7 +37,7 @@ return [
                 /*
                  * Determines if it should avoid unreadable folders.
                  */
-                'ignore_unreadable_directories' => false,
+                'ignore_unreadable_directories' => true,
 
                 /*
                  * This path is used to make directories in resulting zip-file relative
--- config/database.php
+++ config/database.php
@@ -61,6 +61,15 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'dump' => [
+                'excludeTables' => [
+                    'cache',
+                    'cache_locks',
+                    'failed_jobs',
+                    'password_reset_tokens',
+                    'sessions',
+                ]
+            ],
         ],
 
         'parrot' => [
@@ -141,7 +150,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
