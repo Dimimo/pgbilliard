5a6546ef7f04964dd920a734da07c894c917c3e1
Dimitri Mostrey <dmostrey@yahoo.com>
2025-07-23 23:43:21 +0800

    added insert db test

--- tests/Feature/Livewire/Admin/Season/CreateTest.php
+++ tests/Feature/Livewire/Admin/Season/CreateTest.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Livewire\Admin\Season\Create;
+use App\Models\Season;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
     Livewire::test(Create::class)
         ->assertStatus(200);
+});
+
+test('can create a season in the database', function () {
+    Season::factory()->create();
+
+    $current_season = Season::count();
+    expect($current_season)->toBe(1);
 });
