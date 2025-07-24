1715741399cf0b7d22b9e61bdc9d61c0d632b0a5
Dimitri Mostrey <dmostrey@yahoo.com>
2025-07-24 19:57:30 +0800

    testing: the calendar checks if there are dates

--- tests/Feature/Livewire/CalendarTest.php
+++ tests/Feature/Livewire/CalendarTest.php
@@ -3,7 +3,22 @@
 use App\Livewire\Calendar;
 use Livewire\Livewire;
 
+beforeEach(function () {
+    $this->seed(\Database\Seeders\EventSeeder::class);
+    $season = \App\Models\Season::first();
+    session()->put('cycle', $season->cycle);
+    $this->component = Livewire::test(Calendar::class);
+});
+
 it('renders successfully', function () {
-    Livewire::test(Calendar::class)
+    $this->component
         ->assertStatus(200);
+});
+
+it('has dates', function () {
+    $this->component
+        ->assertViewIs('livewire.calendar')
+        ->assertOk()
+        ->assertViewHas('dates')
+        ->assertCount('dates', 4);
 });
