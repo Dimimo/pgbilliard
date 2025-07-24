08fccbb3ed1bf080d4572c90985ac46f8cd4a2d1
Dimitri Mostrey <dmostrey@yahoo.com>
2025-07-24 19:58:40 +0800

    testing: checks in a venue page if the user can edit the database

--- tests/Feature/Livewire/VenueTest.php
+++ tests/Feature/Livewire/VenueTest.php
@@ -3,7 +3,27 @@
 use App\Livewire\Venue;
 use Livewire\Livewire;
 
+beforeEach(function () {
+    $this->owner = \App\Models\User::factory()->create();
+    $this->venue = \App\Models\Venue::factory()->create(['name' => 'my venue', 'user_id' => $this->owner->id]);
+    session(['is_admin' => false]);
+});
+
 it('renders successfully', function () {
     Livewire::test(Venue::class)
         ->assertStatus(200);
+});
+
+it('shows the venue and it can\'t be edited', function () {
+    Livewire::test(Venue::class, ['venue' => $this->venue])
+        ->assertOk()
+        ->assertSee('my venue')
+        ->assertDontSee('Edit this venue');
+});
+
+test('if the owner has access to edit the venue', function () {
+    Livewire::actingAs($this->owner)
+        ->test(Venue::class, ['venue' => $this->venue])
+        ->assertOk()
+        ->assertSee('Edit this venue');
 });
