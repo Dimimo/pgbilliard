a5000c3391effcc6dd1f0022b606268e7b80bcff
Dimitri Mostrey <dmostrey@yahoo.com>
2025-07-25 22:20:46 +0800

    testing: checks if it shows at least one Season

--- tests/Feature/Livewire/CycleAllTest.php
+++ tests/Feature/Livewire/CycleAllTest.php
@@ -7,3 +7,15 @@ it('renders successfully', function () {
     Livewire::test(CycleAll::class)
         ->assertStatus(200);
 });
+
+it('shows the correct component', function () {
+    $this->seed(\Database\Seeders\EventSeeder::class);
+    $expected = "(" . \App\Models\Event::count() . " games, " . \App\Models\Team::count() . " Teams)";
+
+    $response = $this->get('/seasons/all');
+    $response
+        ->assertOk()
+        ->assertSeeVolt('cycle-all')
+        ->assertSee(\App\Models\Season::first()->cycle)
+        ->assertSee($expected);
+});
