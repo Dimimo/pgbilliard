db2a274ad06bf23d7b72e12b7be79f799c093860
Dimitri Mostrey <dmostrey@yahoo.com>
2025-07-25 23:45:48 +0800

    testing: a rudimentary rank tester because of the rankSeeder produces illogical data

--- tests/Feature/Livewire/RankTest.php
+++ tests/Feature/Livewire/RankTest.php
@@ -3,10 +3,26 @@
 use App\Livewire\Rank;
 use Livewire\Livewire;
 
-it('renders successfully', function () {
-    $rank = \App\Models\Rank::factory()->create();
-    session()->put('cycle', $rank->season->cycle);
+beforeEach(function () {
+    $this->season = \App\Models\Season::factory()->create();
+    session(['cycle' => $this->season->cycle]);
+});
 
+it('renders successfully', function () {
     Livewire::test(Rank::class)
         ->assertStatus(200);
+});
+
+it('tests the ranking order and names', function () {
+    $this->seed(\Database\Seeders\RankSeeder::class);
+    $first = \App\Models\Rank::orderByDesc('percentage')->first();
+    $last = \App\Models\Rank::orderBy('played')->first();
+    $playerCount = \App\Models\Player::count();
+
+    Livewire::test(Rank::class)
+        ->assertDontSee($last->user->name)
+        ->call('toggleMedian')
+        ->assertSee($first->player->user->name)
+        ->assertSee($last->player->user->name)
+        ->assertCount('results', $playerCount);
 });
