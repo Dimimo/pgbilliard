e0370d5ac3b8c62b69f296a08da1dca9331e5110
Dimitri Mostrey <dmostrey@yahoo.com>
2025-07-25 23:44:07 +0800

    Revert "testing: user factory changed the name to a simple firstName"
    This reverts commit b25ad0fcf9da4a5b86520c65e60c7a12568c2e26.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,11 +25,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->firstName(),
+            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'gender' => fake()->randomElement(['U', 'M', 'F', 'K']),
-            'last_game' => fake()->dateTimeBetween('-2 years')->format('Y-m-d'),
+            'last_game' => fake()->dateTimeBetween('-2 years', '-6 months')->format('Y-m-d'),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
