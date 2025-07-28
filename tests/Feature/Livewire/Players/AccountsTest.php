1e99b3e581bb9ee852112e5c605b0f99691b40e1
Dimitri Mostrey <dmostrey@yahoo.com>
2025-07-28 11:50:03 +0800

    testing: players overview (for claiming an account)

--- tests/Feature/Livewire/Players/AccountsTest.php
+++ tests/Feature/Livewire/Players/AccountsTest.php
@@ -5,7 +5,17 @@ use Livewire\Livewire;
 
 it('renders successfully', function () {
     \App\Models\Season::factory()->create();
+    $player = \App\Models\Player::factory()->create();
+
+    $response = $this->get('players/accounts');
+
+    $response
+        ->assertOk()
+        ->assertSeeVolt('players.accounts');
 
     Livewire::test(Accounts::class)
-        ->assertStatus(200);
+        ->assertStatus(200)
+        ->assertViewIs('livewire.players.accounts')
+        ->assertSee('Claim an open user account')
+        ->assertViewHas('users');
 });
