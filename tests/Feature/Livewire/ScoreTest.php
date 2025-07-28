c3c7adfce3859400fb5b3b7b30ca6a2b51abccad
Dimitri Mostrey <dmostrey@yahoo.com>
2025-07-28 10:05:02 +0800

    testing: checks if a complete season shows the teams in the right order

--- tests/Feature/Livewire/ScoreTest.php
+++ tests/Feature/Livewire/ScoreTest.php
@@ -8,5 +8,29 @@ it('renders successfully', function () {
     session()->put('cycle', \App\Models\Season::first()->cycle);
 
     Livewire::test(Score::class)
-        ->assertStatus(200);
+        ->assertStatus(200)
+        ->assertSeeVolt('score');
+});
+
+it('loads the score and rank components', function() {
+    $this->seed(\Database\Seeders\EventSeeder::class);
+    session()->put('cycle', \App\Models\Season::first()->cycle);
+
+    $response = $this->get('/');
+
+    $response
+        ->assertOk()
+        ->assertSeeVolt('score')
+        ->assertSeeVolt('rank')
+        ->assertSee('Competition Results');
+});
+
+it('shows the score board of a full Season in the correct order', function() {
+    $this->seed(\Database\Seeders\CompleteSeasonSeeder::class);
+    session()->put('cycle', \App\Models\Season::first()->cycle);
+    $team_names = \App\Models\Team::where('name', '<>', 'BYE')->orderBy('name')->get()->pluck('name')->toArray();
+
+    $response = $this->get('/');
+
+    $response->assertSeeInOrder($team_names);
 });
