14a3a2c574b12f64a7d4b9dd3beb32a753cdf956
Dimitri Mostrey <dmostrey@yahoo.com>
2025-08-10 12:20:20 +0800

    svg directly copied to the file instead of a blade component

--- resources/views/components/navigation/main-links-buttons.blade.php
+++ resources/views/components/navigation/main-links-buttons.blade.php
@@ -1,25 +1,46 @@
 <div class="relative flex justify-center md:mb-1">
     <div class="grid grid-cols-1 gap-2 md:grid-cols-3 md:gap-4">
-        <a href="{{ route('scoreboard') }}" wire:navigate.hover>
+        <a href="{{ route('scoreboard') }}" wire:navigate>
             <div
                 class="flex items-center justify-center rounded-full border border-green-500 bg-green-100 px-3 py-1 text-3xl space-x-2 md:py-3 lg:space-mx-4"
             >
-                <x-svg.table-list-solid color="fill-green-600" size="7" padding=""/>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    class="inline-block w-7 h-7 fill-green-600"
+                >
+                    <path
+                        d="M0 96C0 60.7 28.7 32 64 32l384 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zm64 0l0 64 64 0 0-64L64 96zm384 0L192 96l0 64 256 0 0-64zM64 224l0 64 64 0 0-64-64 0zm384 0l-256 0 0 64 256 0 0-64zM64 352l0 64 64 0 0-64-64 0zm384 0l-256 0 0 64 256 0 0-64z"/>
+                </svg>
                 <div>{{__('Scoreboard')}}</div>
             </div>
         </a>
-        <a href="{{ route('calendar') }}" wire:navigate.hover>
+        <a href="{{ route('calendar') }}" wire:navigate>
             <div
                 class="flex items-center justify-center rounded-full border border-blue-600 bg-blue-100 px-3 py-1 text-3xl space-x-2 md:py-3 lg:space-mx-4">
-                <x-svg.calendar-days-solid color="fill-blue-600" size="7" padding=""/>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512"
+                    class="inline-block w-7 h-7 fill-blue-600"
+                >
+                    <path
+                        d="M128 0c17.7 0 32 14.3 32 32l0 32 128 0 0-32c0-17.7 14.3-32 32-32s32 14.3 32 32l0 32 48 0c26.5 0 48 21.5 48 48l0 48L0 160l0-48C0 85.5 21.5 64 48 64l48 0 0-32c0-17.7 14.3-32 32-32zM0 192l448 0 0 272c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 192zm64 80l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm128 0l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zM64 400l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zm112 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16z"/>
+                </svg>
                 <div>{{__('Calendar')}}</div>
             </div>
         </a>
-        <a href="{{ route('teams.index') }}" wire:navigate.hover>
+        <a href="{{ route('teams.index') }}" wire:navigate>
             <div
                 class="flex items-center justify-center rounded-full border border-amber-500 bg-amber-100 px-3 py-1 text-3xl space-x-2 md:py-3 lg:space-mx-4"
             >
-                <x-svg.users-solid color="fill-amber-500" size="7" padding=""/>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 640 512"
+                    class="inline-block w-7 h-7 fill-amber-500"
+                >
+                    <path
+                        d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192l42.7 0c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0L21.3 320C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7l42.7 0C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3l-213.3 0zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352l117.3 0C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7l-330.7 0c-14.7 0-26.7-11.9-26.7-26.7z"/>
+                </svg>
                 <div>{{__('Teams')}}</div>
             </div>
         </a>
