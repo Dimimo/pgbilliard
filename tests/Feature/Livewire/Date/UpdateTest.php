da8f6944319d8d5f72637f06e54e8f7a2324a04d
Dimitri Mostrey <dmostrey@yahoo.com>
2025-08-15 02:08:06 +0800

    testing: event score update test

--- tests/Feature/Livewire/Date/UpdateTest.php
+++ tests/Feature/Livewire/Date/UpdateTest.php
@@ -3,10 +3,39 @@
 use App\Livewire\Date\Update;
 use Livewire\Livewire;
 
-it('renders successfully', function () {
+beforeEach(function () {
+    $this->member = \App\Models\User::factory()->create();
     $this->seed(\Database\Seeders\EventSeeder::class);
-    $event = \App\Models\Event::latest()->first();
+    $this->game = \App\Models\Event::find(2);
+    session(['is_admin' => false]);
+});
 
-    Livewire::test(Update::class, ['event' => $event])
-        ->assertStatus(200);
+it('renders successfully', function () {
+    Livewire::actingAs($this->member)
+        ->test(Update::class, ['event' => $this->game])
+        ->assertStatus(200)
+        ->assertViewIs('livewire.date.update')
+        ->assertViewHas('event', $this->game)
+        ->assertViewHas('confirmed', false)
+        ->assertViewHas('score1');
+});
+
+it('can change a score and dispatches a list refresh request', function () {
+    Livewire::actingAs($this->member)
+        ->test(Update::class, ['event' => $this->game])
+        ->call('change', 'score1')
+        ->assertDispatched('refresh-list')
+        ->assertViewHas('score1', ++$this->game->score1);
+});
+
+it('shows errors if the score is less than 0 or more than 15', function () {
+    Livewire::actingAs($this->member)
+        ->test(Update::class, ['event' => $this->game])
+        ->set('score2', 0)
+        ->call('change', 'score2', 'decrement')
+        ->assertViewHas('score2', -1)
+        ->assertHasErrors(['score2' => ['between:0,15']])
+        ->set('score2', 14)
+        ->call('change', 'score2')
+        ->assertHasErrors('score1');
 });
