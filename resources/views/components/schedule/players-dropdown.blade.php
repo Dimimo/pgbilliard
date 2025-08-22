f78e1c0cd2c5db7834c99f97be4ce7358bee8c4b
Dimitri Mostrey <dmostrey@yahoo.com>
2025-08-22 23:28:49 +0800

    possible bug fix in where the players collection is empty

--- resources/views/components/schedule/players-dropdown.blade.php
+++ resources/views/components/schedule/players-dropdown.blade.php
@@ -12,59 +12,61 @@
             </x-forms.action-message>
             <div>{{ Str::ucfirst($place) }} {{__('Team')}}</div>
         </div>
-        @for($i=1;$i<=$players->count();$i++)
-            @php
-                $old_position_player_id = $matrix->where('rank', $i)->first()?->player->id;
-            @endphp
+        @if(!empty($players))
+            @for($i=1;$i<=$players->count();$i++)
+                @php
+                    $old_position_player_id = $matrix->where('rank', $i)->first()?->player->id;
+                @endphp
 
-            <div @class([
+                <div @class([
                 'mb-1 flex items-center justify-end',
                 'flex-row-reverse' => $place === 'visit',
             ])
-                 wire:key="player-{{$players->first()->team->id}}-{{$matrix->count()}}"
-            >
-                @if ($can_update_players && Gate::authorize('update', $event))
-                    <div class="mx-2">
-                        @if ($i <= 4)
-                            {{Str::ucfirst($place)}} {{ $i }}
-                        @else
-                            {{__('Reserve')}}
-                        @endif
-                    </div>
-                    <label>
-                        <select
-                            class="text-sm"
-                            wire:change="playerSelected($event.target.value, {{$i}}, '{{$place}}', {{$old_position_player_id}})"
-                        >
-                            <option value="0">
-                                -- {{__('select')}} --
-                            </option>
-                            @foreach($players as $player)
-                                <option
-                                    wire:key="selected-{{$i}}-{{$matrix->count()}}"
-                                    @selected($old_position_player_id === $player->id)
-                                    value="{{$player->id}}"
-                                >
-                                    {{$player->name}}
+                     wire:key="player-{{$players->first()->team->id}}-{{$matrix->count()}}"
+                >
+                    @if ($can_update_players && Gate::authorize('update', $event))
+                        <div class="mx-2">
+                            @if ($i <= 4)
+                                {{Str::ucfirst($place)}} {{ $i }}
+                            @else
+                                {{__('Reserve')}}
+                            @endif
+                        </div>
+                        <label>
+                            <select
+                                class="text-sm"
+                                wire:change="playerSelected($event.target.value, {{$i}}, '{{$place}}', {{$old_position_player_id}})"
+                            >
+                                <option value="0">
+                                    -- {{__('select')}} --
                                 </option>
-                            @endforeach
-                        </select>
-                    </label>
-                @elseif ($matrix->where('rank', $i)->count())
-                    @if($matrix->where('rank', $i)->first()->home)
-                        <div class="mx-2 text-lg">{{$matrix->where('rank', $i)->first()->player?->name}}</div>
-                        <div class="inline-flex items-center rounded-full border border-indigo-400 bg-indigo-100 px-2 py-1 font-bold leading-4">
-                            {{$i}}
-                        </div>
-                    @else
-                        <div class="mx-2 text-lg">{{$matrix->where('rank', $i)->first()->player?->name}}</div>
-                        <div class="inline-flex items-center rounded-full border border-green-500 bg-green-100 px-2 py-1 font-bold leading-4">
-                            {{$i}}
-                        </div>
+                                @foreach($players as $player)
+                                    <option
+                                        wire:key="selected-{{$i}}-{{$matrix->count()}}"
+                                        @selected($old_position_player_id === $player->id)
+                                        value="{{$player->id}}"
+                                    >
+                                        {{$player->name}}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </label>
+                    @elseif ($matrix->where('rank', $i)->count())
+                        @if($matrix->where('rank', $i)->first()->home)
+                            <div class="mx-2 text-lg">{{$matrix->where('rank', $i)->first()->player?->name}}</div>
+                            <div class="inline-flex items-center rounded-full border border-indigo-400 bg-indigo-100 px-2 py-1 font-bold leading-4">
+                                {{$i}}
+                            </div>
+                        @else
+                            <div class="mx-2 text-lg">{{$matrix->where('rank', $i)->first()->player?->name}}</div>
+                            <div class="inline-flex items-center rounded-full border border-green-500 bg-green-100 px-2 py-1 font-bold leading-4">
+                                {{$i}}
+                            </div>
+                        @endif
                     @endif
-                @endif
-            </div>
-        @endfor
+                </div>
+            @endfor
+        @endif
     </div>
     @if ($can_update_players && Gate::authorize('update', $event))
         <div @class([
