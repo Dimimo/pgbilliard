a0e51e1faf0a44b43e5ed36ef235028edb71d346
Dimitri Mostrey <dmostrey@yahoo.com>
2025-09-02 07:46:05 +0800

    the folio service provider boot method simplified

--- app/Providers/FolioServiceProvider.php
+++ app/Providers/FolioServiceProvider.php
@@ -20,13 +20,10 @@ class FolioServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Folio::path(resource_path('views/pages/admin'))->uri('/admin')->middleware([
-            '*' => [
+        Folio::path(resource_path('views/pages'))->middleware([
+            'admin/*' => [
                 'auth',
             ],
-        ]);
-        Folio::path(resource_path('views/pages'))->middleware([
-            '*' => [],
         ]);
     }
 }
