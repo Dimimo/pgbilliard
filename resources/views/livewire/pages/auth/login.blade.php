554d35d7de14024fbe45440201eaf728ddf6f2a2
Dimitri Mostrey <dmostrey@yahoo.com>
2025-09-06 01:12:20 +0800

    login: added the show password check

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -11,12 +11,13 @@ use function Livewire\Volt\state;
 
 layout('components.layout');
 
-state(['email' => '', 'password' => '', 'remember' => false]);
+state(['email' => '', 'password' => '', 'remember' => false, 'show_password' => false]);
 
 rules([
     'email' => ['required', 'string', 'email'],
     'password' => ['required', 'string'],
     'remember' => ['boolean'],
+    'show_password' => ['boolean'],
 ]);
 
 $login = function () {
@@ -32,7 +33,7 @@ $login = function () {
             ]),
         ]);
     }
-    if (!auth()->attempt($this->only(['email', 'password'], $this->remember))) {
+    if (!auth()->attempt($this->only(['email', 'password']), $this->remember)) {
         RateLimiter::hit($throttleKey);
         throw ValidationException::withMessages([
             'email' => trans('auth.failed'),
@@ -44,6 +45,10 @@ $login = function () {
     auth()->user()->touch('updated_at');
     $this->redirect(session('url.intended', RouteServiceProvider::HOME));
 };
+
+$toggle_password = function () {
+    $this->show_password = !$this->show_password;
+};
 ?>
 
 <div class="mx-auto">
@@ -54,8 +59,15 @@ $login = function () {
         <!-- Email Address -->
         <div>
             <x-forms.input-label for="email" :value="__('Email')"/>
-            <x-forms.text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus
-                                autocomplete="username"/>
+            <x-forms.text-input
+                wire:model="email"
+                id="email"
+                class="block mt-1 w-full"
+                type="email"
+                name="email"
+                required autofocus
+                autocomplete="username"
+            />
             <x-forms.input-error :messages="$errors->get('email')" class="mt-2"/>
         </div>
 
@@ -63,21 +75,43 @@ $login = function () {
         <div class="mt-4">
             <x-forms.input-label for="password" :value="__('Password')"/>
 
-            <x-forms.text-input wire:model="password" id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password"/>
+            <x-forms.text-input
+                wire:model="password" id="password" class="block mt-1 w-full"
+                type="{{$show_password ? 'text' : 'password'}}"
+                name="password"
+                data-np-uuid="whatever-text-you-want-here"
+                required autocomplete="current-password"
+            />
 
             <x-forms.input-error :messages="$errors->get('password')" class="mt-2"/>
+
         </div>
 
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember" class="inline-flex items-center">
-                <input wire:model="remember" id="remember" type="checkbox"
-                       class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
-                       name="remember">
+                <input
+                    wire:model="remember"
+                    id="remember"
+                    type="checkbox"
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                    name="remember"
+                >
                 <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <!-- Show/Hide Password -->
+        <div class="block mt-4">
+            <label for="show_password" class="inline-flex items-center">
+                <input
+                    id="show_password"
+                    type="checkbox"
+                    wire:change="toggle_password()"
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                    name="show_password"
+                >
+                <span class="ml-2 text-sm text-gray-600">{{ __('Show/Hide password') }}</span>
             </label>
         </div>
 
