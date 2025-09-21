1629e16768d95e0fe2c74d9d71b5706f58e14aa1
Dimitri Mostrey <dmostrey@yahoo.com>
2025-09-21 11:07:15 +0800

    constant type is missing

--- app/Constants.php
+++ app/Constants.php
@@ -4,22 +4,21 @@ namespace App;
 
 class Constants
 {
-    public const MAX_TEAM_PLAYERS = 6;
-    public const STARTING_DAY = 'Wednesday';
-    public const SHOW_INDIVIDUAL_RANK_COUNT = 20;
-    public const VENUECHARS = 24;
-    public const USERCHARS = 24;
-    public const TEAMCHARS = 16;
-    public const PHONECHARS = 24;
-    public const FORUM_TITLE = 80;
-    public const FORUM_BODY = 5000;
-    public const COMMENT_BODY = 250;
-    public const CHATROOM_TITLE = 12;
-    public const CHATROOM_MESSAGE = 150;
-    public const DATE_TITLE = 20;
-    public const DATEFORMAT_START = 12;
-    public const DATEFORMAT_END = 20;
-    public const SCHEDULE_DETAILS = 256;
-    public const EMAIL_TITLE = 80;
-    public const EMAIL_BODY = 5000;
+    public const int MAX_TEAM_PLAYERS = 6;
+    public const string STARTING_DAY = 'Wednesday';
+    public const int VENUECHARS = 24;
+    public const int USERCHARS = 24;
+    public const int TEAMCHARS = 16;
+    public const int PHONECHARS = 24;
+    public const int FORUM_TITLE = 80;
+    public const int FORUM_BODY = 5000;
+    public const int COMMENT_BODY = 250;
+    public const int CHATROOM_TITLE = 12;
+    public const int CHATROOM_MESSAGE = 150;
+    public const int DATE_TITLE = 20;
+    public const int DATEFORMAT_START = 12;
+    public const int DATEFORMAT_END = 20;
+    public const int SCHEDULE_DETAILS = 256;
+    public const int EMAIL_TITLE = 80;
+    public const int EMAIL_BODY = 5000;
 }
