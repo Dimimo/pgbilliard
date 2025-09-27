c67582e86e01bf7a07dd36956471e568bb13b1b9
Dimitri Mostrey <dmostrey@yahoo.com>
2025-09-27 18:32:53 +0800

    score skeleton improved version

--- app/Skeletons/ScoreSkeleton.php
+++ app/Skeletons/ScoreSkeleton.php
@@ -24,74 +24,75 @@ class ScoreSkeleton
                     <button class="px-4 py-2 bg-gray-300 animate-pulse rounded">Loading...</button>
                 </div>
 
-                <table class="table-collapse my-2 min-w-full bg-transparent md:my-4">
-                    <thead class="whitespace-nowrap">
-                        <tr>
-                            <th class="bg-gray-300 p-2 text-center text-gray-900 animate-pulse rounded">#</th>
-                            <th class="bg-blue-300 p-2 text-left text-gray-900 animate-pulse rounded">Team</th>
-                            <th class="bg-amber-300 p-2 text-left text-gray-900 animate-pulse rounded">Last game</th>
-                            <th class="bg-yellow-200 p-2 text-center text-gray-900 animate-pulse rounded">Score</th>
-                        </tr>
-                   </thead>
-                   <tbody class="whitespace-nowrap">
-                        <tr class="h-8">
-                            <td class="bg-gray-200 p-2 text-center text-gray-900 animate-pulse">
-                                <div class="h-4 w-8 bg-gray-300 rounded"></div>
-                            </td>
-                            <td class="p-2 bg-gray-200 animate-pulse">
-                                <div class="h-4 w-32 bg-gray-300 rounded"></div>
-                            </td>
-                            <td class="bg-gray-200 p-2 text-gray-900 animate-pulse">
-                                <div class="h-4 w-32 bg-gray-300 rounded"></div>
-                            </td>
-                            <td class="p-2 text-center">
-                                <div class="h-4 w-32 bg-gray-300 rounded"></div>
-                            </td>
-                        </tr>
-                        <tr class="h-8">
-                            <td class="bg-gray-200 p-2 text-center text-gray-900 animate-pulse">
-                                <div class="h-4 w-8 bg-gray-300 rounded"></div>
-                            </td>
-                            <td class="p-2 bg-gray-200 animate-pulse">
-                                <div class="h-4 w-32 bg-gray-300 rounded"></div>
-                            </td>
-                            <td class="bg-gray-200 p-2 text-gray-900 animate-pulse">
-                                <div class="h-4 w-32 bg-gray-300 rounded"></div>
-                            </td>
-                            <td class="p-2 text-center">
-                                <div class="h-4 w-32 bg-gray-300 rounded"></div>
-                            </td>
-                        </tr>
-                        <tr class="h-8">
-                            <td class="bg-gray-200 p-2 text-center text-gray-900 animate-pulse">
-                                <div class="h-4 w-8 bg-gray-300 rounded"></div>
-                            </td>
-                            <td class="p-2 bg-gray-200 animate-pulse">
-                                <div class="h-4 w-32 bg-gray-300 rounded"></div>
-                            </td>
-                            <td class="bg-gray-200 p-2 text-gray-900 animate-pulse">
-                                <div class="h-4 w-32 bg-gray-300 rounded"></div>
-                            </td>
-                            <td class="p-2 text-center">
-                                <div class="h-4 w-32 bg-gray-300 rounded"></div>
-                            </td>
-                        </tr>
-                        <tr class="h-8">
-                            <td class="bg-gray-200 p-2 text-center text-gray-900 animate-pulse">
-                                <div class="h-4 w-8 bg-gray-300 rounded"></div>
-                            </td>
-                            <td class="p-2 bg-gray-200 animate-pulse">
-                                <div class="h-4 w-32 bg-gray-300 rounded"></div>
-                            </td>
-                            <td class="bg-gray-200 p-2 text-gray-900 animate-pulse">
-                                <div class="h-4 w-32 bg-gray-300 rounded"></div>
-                            </td>
-                            <td class="p-2 text-center">
-                                <div class="h-4 w-32 bg-gray-300 rounded"></div>
-                            </td>
-                        </tr>
+
+            <table class="table-collapse my-2 min-w-full bg-transparent md:my-4">
+                <thead class="whitespace-nowrap">
+                <tr>
+                    <th class="w-3 bg-gray-300 p-2 text-center text-gray-900 animate-pulse">#</th>
+                    <th class="bg-blue-300 p-2 text-left text-gray-900 animate-pulse">Team</th>
+                    <th class="bg-amber-300 p-2 text-left text-gray-900 animate-pulse">Last game</th>
+                    <th class="bg-yellow-200 p-2 text-center text-gray-900 animate-pulse">Score</th>
+                </tr>
+                </thead>
+                <tbody class="whitespace-nowrap">
+                <tr class="h-12">
+                    <td class="bg-gray-200 p-2 text-center text-gray-900 animate-pulse font-bold">
+                        1
+                    </td>
+                    <td class="p-2 bg-blue-100 animate-pulse">
+                        <div class="h-4 w-32 bg-gray-300"></div>
+                    </td>
+                    <td class="bg-amber-200 p-2 text-gray-900 animate-pulse">
+                        <div class="h-4 w-32 bg-gray-300"></div>
+                    </td>
+                    <td class="p-2 text-center">
+                        <div class="h-4 w-32 bg-gray-300"></div>
+                    </td>
+                </tr>
+                <tr class="h-12">
+                    <td class="bg-gray-200 p-2 text-center text-gray-900 animate-pulse font-bold">
+                        2
+                    </td>
+                    <td class="p-2 bg-gray-200 animate-pulse">
+                        <div class="h-4 w-32 bg-gray-300"></div>
+                    </td>
+                    <td class="bg-gray-200 p-2 text-gray-900 animate-pulse">
+                        <div class="h-4 w-32 bg-gray-300"></div>
+                    </td>
+                    <td class="p-2 text-center">
+                        <div class="h-4 w-32 bg-gray-300"></div>
+                    </td>
+                </tr>
+                <tr class="h-12">
+                    <td class="bg-gray-200 p-2 text-center text-gray-900 animate-pulse font-bold">
+                        3
+                    </td>
+                    <td class="p-2 bg-gray-200 animate-pulse">
+                        <div class="h-4 w-32 bg-gray-300"></div>
+                    </td>
+                    <td class="bg-gray-200 p-2 text-gray-900 animate-pulse">
+                        <div class="h-4 w-32 bg-gray-300"></div>
+                    </td>
+                    <td class="p-2 text-center">
+                        <div class="h-4 w-32 bg-gray-300"></div>
+                    </td>
+                </tr>
+                <tr class="h-12">
+                    <td class="bg-gray-200 p-2 text-center text-gray-900 animate-pulse font-bold">
+                        4
+                    </td>
+                    <td class="p-2 bg-gray-200 animate-pulse">
+                        <div class="h-4 w-32 bg-gray-300"></div>
+                    </td>
+                    <td class="bg-gray-200 p-2 text-gray-900 animate-pulse">
+                        <div class="h-4 w-32 bg-gray-300"></div>
+                    </td>
+                    <td class="p-2 text-center">
+                        <div class="h-4 w-32 bg-gray-300"></div>
+                    </td>
+                </tr>
                 </tbody>
-               </table>
+            </table>
            </div>
         </div>
 
