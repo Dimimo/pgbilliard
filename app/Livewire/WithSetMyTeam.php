f88c5c2500ffeac63281a774d1ea1721caa15e0f
Dimitri Mostrey <dmostrey@yahoo.com>
2025-09-30 09:17:27 +0800

    the session value of 'my_team' is set as a middleware, improved functionality during the session

--- app/Livewire/WithSetMyTeam.php
+++ app/Livewire/WithSetMyTeam.php
@@ -9,20 +9,20 @@ trait WithSetMyTeam
     public function mountWithSetMyTeam(): void
     {
         $this->my_team = session('my_team');
-        if (!$this->my_team && auth()->check()) {
-            $this->my_team = $this->setMyTeamBasedOnUser();
-        }
     }
 
     public function setMyTeam(int $id): void
     {
         session('my_team') === $id
-            ? session()->put('my_team', $this->my_team = 0)
+            ? session()->put('my_team', $this->my_team = $this->setMyTeamBasedOnUser())
             : session()->put('my_team', $this->my_team = $id);
     }
 
     private function setMyTeamBasedOnUser(): ?int
     {
+        if (!auth()->check()) {
+            return null;
+        }
         // @phpstan-ignore-next-line
         $date = $this->date ?? $this->dates->first();
         return $date->getTeam(auth()->user())?->id;
