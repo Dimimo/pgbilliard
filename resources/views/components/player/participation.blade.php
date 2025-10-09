48c9979c5984af523fb23ed401f9971911b9eb99
Dimitri Mostrey <dmostrey@yahoo.com>
2025-10-09 15:30:22 +0800

    personalized in case it's not shown as the dashboard

--- resources/views/components/player/participation.blade.php
+++ resources/views/components/player/participation.blade.php
@@ -1,11 +1,29 @@
 @props(['team', 'player', 'rank'])
+@php
+    $your = request()->routeIs('dashboard') ? 'Your' : "";
+@endphp
 
 <div class="rounded-lg bg-indigo-50 p-4">
     @if ($team)
-        <div class="mb-4 text-lg">{{ __('Your participation') }}:</div>
+        @if (request()->routeIs('dashboard'))
+            <div class="mb-4 text-xl">
+                {{ Str::replace('Your', $your, __('Your participation')) }}:
+            </div>
+        @endif
+
+        <div class="my-4 text-lg">
+            <a href="{{ route('rank') }}" class="link inline-block text-blue-800" wire:navigate>
+                {{ Str::replace('Your', $your, __('Your current individual rank is')) }}
+            </a>
+            <span
+                class="m-1 rounded-full border border-green-500 bg-green-100 px-2 py-0.5 font-bold"
+            >
+                {{ $rank }}
+            </span>
+        </div>
         <div class="mb-4">
             <div>
-                {{ __('You play for') }}
+                {{ Str::replace('Your', $your, __('Your Team')) }}:
                 <a
                     href="{{ route('teams.show', ['team' => $team]) }}"
                     class="link inline-block text-blue-800"
@@ -14,22 +32,7 @@
                     {{ $team->name }}
                 </a>
             </div>
-            <div class="my-4">
-                {{ __('Your current') }}
-                <a
-                    href="{{ route('rank') }}"
-                    class="link inline-block text-blue-800"
-                    wire:navigate
-                >
-                    {{ __('individual rank is') }}
-                </a>
-                <span
-                    class="m-1 rounded-full border border-green-500 bg-green-100 px-2 py-1 font-bold"
-                >
-                    {{ $rank }}
-                </span>
-            </div>
-            @if ($player->captain)
+            @if (request()->routeIs('dashboard') && $player->captain)
                 <div>
                     {{ __('You are the Captain, you can') }}
                     <a
@@ -42,13 +45,28 @@
                 </div>
             @endif
         </div>
-        <div class="text-lg">{{ __('Your team members are') }}:</div>
+        <div class="mb-2 text-lg">
+            {{ Str::replace('Your', $your, __('Your team members are')) }}:
+        </div>
         <ul class="list-inside list-disc">
             @foreach ($team->players->where('active', true)->sortBy('name')->sortByDesc('captain') as $member)
-                <li>{{ $member->user->name }} {{ $member->captain ? 'is captain' : '' }}</li>
+                <li class="list-item">
+                    <span class="inline-block">
+                        <a
+                            class="link text-blue-800"
+                            href="{{ route('players.show', ['player' => $member->id]) }}"
+                            wire:navigate
+                        >
+                            {{ $member->user->name }}
+                        </a>
+                    </span>
+                    <span>
+                        {{ $member->captain ? __('is Captain') : '' }}
+                    </span>
+                </li>
             @endforeach
         </ul>
-    @else
+    @elseif (request()->routeIs('dashboard'))
         <div class="mb-4">
             {{ __("You don't play for a team. If you think this is an error") }}
             <a
