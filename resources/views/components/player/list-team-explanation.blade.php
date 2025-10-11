208dcbe4533c1c9b9f45b07bc7c9e798492fe5b0
Dimitri Mostrey <dmostrey@yahoo.com>
2025-10-11 12:45:40 +0800

    rank: moved the explanation to a blade template

---
+++ resources/views/components/player/list-team-explanation.blade.php
@@ -0,0 +1,40 @@
+@props(['median', 'count'])
+
+<div>
+    <div>
+        The list shows
+        <span class="font-bold">the {{ $median }} most active players</span>
+        of a total of {{ $count }} players in this Season.
+    </div>
+    <div class="mb-4">Optionally, you can see the list of all players.</div>
+    <div class="mb-4">The calculation of the percentage:</div>
+    <div class="mb-4">
+        <math xmlns="http://www.w3.org/1998/Math/MathML">
+            <mrow>
+                <mo>(</mo>
+                <mfrac>
+                    <mtext>{{__('Games Won')}}</mtext>
+                    <mtext>{{__('Total Games')}}</mtext>
+                </mfrac>
+                <mo>)</mo>
+                <mo>
+                    <x-svg.xmark-solid color="fill-black" size="4" padding="mt-1" />
+                </mo>
+                <mo>(</mo>
+                <mfrac>
+                    <mtext>{{__('Days Participated')}}</mtext>
+                    <mtext>{{__('Max Playing Days')}}</mtext>
+                </mfrac>
+                <mo>)</mo>
+                <mo>
+                    <x-svg.xmark-solid color="fill-black" size="4" padding="mt-1" />
+                </mo>
+                <mn>100</mn>
+            </mrow>
+        </math>
+    </div>
+    <div class="mb-4">
+        <span class="font-bold">Hint:</span>
+        {{ __('click on a name to see the game details') }}
+    </div>
+</div>
--- resources/views/livewire/rank.blade.php
+++ resources/views/livewire/rank.blade.php
@@ -18,42 +18,8 @@
         @endif
     @else
         <div class="m-auto mb-4 rounded-lg border border-indigo-400 bg-indigo-50 p-2 text-center">
-            <div>
-                The list shows
-                <span class="font-bold">the {{ $median }} most active players</span>
-                of a total of {{ $results->count() }} players in this Season.
-            </div>
-            <div class="mb-4">Optionally, you can see the list of all players.</div>
-            <div class="mb-4">The calculation of the percentage:</div>
-            <div class="mb-4">
-                <math xmlns="http://www.w3.org/1998/Math/MathML">
-                    <mrow>
-                        <mo>(</mo>
-                        <mfrac>
-                            <mtext>{{__('Games Won')}}</mtext>
-                            <mtext>{{__('Total Games')}}</mtext>
-                        </mfrac>
-                        <mo>)</mo>
-                        <mo>
-                            <x-svg.xmark-solid color="fill-black" size="4" padding="mt-1" />
-                        </mo>
-                        <mo>(</mo>
-                        <mfrac>
-                            <mtext>{{__('Days Participated')}}</mtext>
-                            <mtext>{{__('Max Playing Days')}}</mtext>
-                        </mfrac>
-                        <mo>)</mo>
-                        <mo>
-                            <x-svg.xmark-solid color="fill-black" size="4" padding="mt-1" />
-                        </mo>
-                        <mn>100</mn>
-                    </mrow>
-                </math>
-            </div>
-            <div class="mb-4">
-                <span class="font-bold">Hint:</span>
-                {{ __('click on a name to see the game details') }}
-            </div>
+            <x-player.list-team-explanation :median="$median" :count="$results->count()" />
+
             @if (session('is_admin'))
                 <div class="text-center">
                     <button
