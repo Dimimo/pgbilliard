34c3c7acbb1dd84dd70d15ed6fe4669e3e63c8ad
Dimitri Mostrey <dmostrey@yahoo.com>
2025-10-14 18:45:36 +0800

    the team ordering trait has added comments for clarification

--- app/Livewire/WithTeamOrdering.php
+++ app/Livewire/WithTeamOrdering.php
@@ -6,13 +6,38 @@ use App\Models\Season;
 use App\Models\Team;
 use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * a trait that handles adding or removing teams in the current season
+ */
 trait WithTeamOrdering
 {
+    /**
+     * @var Collection
+     */
     public Collection $teams;
+    /**
+     * a collection of possible team selection for the current season
+     *
+     * @var Collection
+     */
     public Collection $dropdown_teams;
+    /**
+     * the number of teams can come from a session value or is set to an arbitrary number of teams (usually 6)
+     *
+     * @var int
+     */
     public int $number_of_teams;
+    /**
+     * @var int
+     */
     public int $i = 1;
 
+    /**
+     * push a selected team into the current season and sort by name
+     *
+     * @param Team $team
+     * @return void
+     */
     private function pushAndSortTeams(Team $team): void
     {
         $this->teams->push($team);
@@ -23,6 +48,11 @@ trait WithTeamOrdering
         $this->changeNumberOfTeams();
     }
 
+    /**
+     * Get the teams from the last 2 most recent seasons
+     *
+     * @return Collection
+     */
     private function getDropdownTeams(): Collection
     {
         $season_ids = Season::query()->orderByDesc('cycle')->skip(1)->take(2)->pluck('id')->toArray();
@@ -31,6 +61,12 @@ trait WithTeamOrdering
         return Team::query()->whereIn('season_id', $season_ids)->whereNotIn('name', $new_team_names)->orderBy('name')->orderByDesc('season_id')->get()->unique('name');
     }
 
+    /**
+     * called if a team is added or removed
+     *
+     * @param bool $add
+     * @return void
+     */
     private function changeNumberOfTeams(bool $add = true): void
     {
         $this->number_of_teams = $add ? $this->number_of_teams + 1 : $this->number_of_teams - 1;
