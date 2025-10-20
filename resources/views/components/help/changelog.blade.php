7cae7c9583f529602dffd221073575c85f05bef6
Dimitri Mostrey <dmostrey@yahoo.com>
2025-10-20 10:41:31 +0800

    updated changelog

--- resources/views/components/help/changelog.blade.php
+++ resources/views/components/help/changelog.blade.php
@@ -115,6 +115,12 @@
         <li>
             Oct 3, 2025: the individual scoreboard is finally ready and out of beta
         </li>
+        <li>
+            Oct 8, 2025: rewritten the individual scores mechanism to make it less error prawn
+        </li>
+        <li>
+            Oct 10, 2025: added a shortcut to the individual games
+        </li>
     </ul>
 </div>
 <div class="mt-4">
