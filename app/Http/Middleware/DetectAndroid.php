d8198256eb984409935301557c93257d8f557239
Dimitri Mostrey <dmostrey@yahoo.com>
2025-10-22 13:01:13 +0800

    Refactor Android device detection to use request session methods and update previous URL logic

--- app/Http/Middleware/DetectAndroid.php
+++ app/Http/Middleware/DetectAndroid.php
@@ -19,9 +19,13 @@ class DetectAndroid
         if (stripos($request->header('User-Agent'), 'android') !== false) {
             // User is on an Android device
             // You can perform actions here, like setting a session variable or logging
-            session(['is_android' => true]);
+            $request->session()->put('is_android', true);
+            if ($request->url() !== $request->session()->previousUrl())
+            {
+                $request->session()->setPreviousUrl($request->url());
+            }
         } else {
-            session(['is_android' => false]);
+            $request->session()->put('is_android', false);
         }
 
         return $next($request);
