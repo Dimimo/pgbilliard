5b96d4f1ba0f3073e1314d9537355413510ec0ec
Dimitri Mostrey <dmostrey@yahoo.com>
2025-10-30 17:02:48 +0800

    Add initial Scribe configuration for API documentation generation

--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -5,10 +5,16 @@ namespace App\Http\Resources;
 use App\Models\Event;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Knuckles\Scribe\Attributes\Endpoint;
+use Knuckles\Scribe\Attributes\Group;
+use Knuckles\Scribe\Attributes\ResponseFromApiResource;
 
 /** @mixin Event */
+#[Group(name: 'Events and Games', description: 'Here are the games', authenticated: false)]
 class EventResource extends JsonResource
 {
+    #[ResponseFromApiResource(name: 'Events and Games', model: Event::class, description: 'Shows the games and results', collection: false)]
+    #[Endpoint('Events and Games', 'Shows the games and results', false)]
     public function toArray(Request $request): array
     {
         return [
