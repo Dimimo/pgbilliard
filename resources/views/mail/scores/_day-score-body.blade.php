4ed9047e6788c6bbbd7a2ab3be896f2f60f16dbe
Dimitri Mostrey <dmostrey@yahoo.com>
2025-11-26 18:51:30 +0800

    Fix table formatting in day score email template

--- resources/views/mail/scores/_day-score-body.blade.php
+++ resources/views/mail/scores/_day-score-body.blade.php
@@ -1,8 +1,8 @@
 <x-mail::table>
-| Home Team | Visitors | Score | |:---------:|:---------:|:---------:|
+| Home Team | Visitors | Score |
+|:---------:|:---------:|:---------:|
 @foreach ($date->events as $event)
-| {{ $event->team_1->name }} | {{ $event->team_2->name }} | {{ $event->score1 }} -
-{{ $event->score2 }} |
+| {{ $event->team_1->name }} | {{ $event->team_2->name }} | {{ $event->score1 }} - {{ $event->score2 }} |
 @endforeach
 </x-mail::table>
 
