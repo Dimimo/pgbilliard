585a1b6e971e746a930775a47483a791743b920e
Dimitri Mostrey <dmostrey@yahoo.com>
2026-01-22 09:06:26 +0800

    Change schedule format order to descending and add cursor pointer for buttons

--- resources/views/livewire/date/schedule-format-chooser.blade.php
+++ resources/views/livewire/date/schedule-format-chooser.blade.php
@@ -4,9 +4,9 @@
             <div class="my-8" wire:transition.duration.250ms>
                 <x-forms.sub-title title="{{__('Choose a day games format')}}">
                     <div class="flex justify-center">
-                        @foreach (App\Models\Format::orderBy('name')->get() as $f)
+                        @foreach (App\Models\Format::orderByDesc('name')->get() as $f)
                             <button
-                                class="m-2 w-full rounded-lg border border-green-500 bg-green-100 p-2 text-left"
+                                class="m-2 w-full cursor-pointer rounded-lg border border-green-500 bg-green-100 p-2 text-left"
                                 wire:click="$parent.formatChosen({{ $f->id }})"
                             >
                                 <div class="text-lg">{{ $f->name }}</div>
