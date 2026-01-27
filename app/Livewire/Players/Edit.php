2a9e8fa95a5d141b7b24f77a2f92f8c14aad7e0f
Dimitri Mostrey <dmostrey@yahoo.com>
2026-01-27 13:05:40 +0800

    Fix return type of the player loading function in the Players Edit component

--- app/Livewire/Players/Edit.php
+++ app/Livewire/Players/Edit.php
@@ -101,7 +101,7 @@ class Edit extends Component
         $this->available_players = $this->loadUsersCollection(array_keys($this->occupied_players));
         // add the name of the last team and last played game in the dropdown list
         $this->available_players->each(
-            function (User $q): void {
+            function (User $q): ?Player {
                 $player = Player::query()
                     ->where('user_id', $q->id)
                     ->with('team')
