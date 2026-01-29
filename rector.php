615cd4c4700ee747d6e55761e563bddb4a580c48
Dimitri Mostrey <dmostrey@yahoo.com>
2026-01-29 14:12:59 +0800

    Update Rector configuration for Laravel sets and include additional quality rules

--- rector.php
+++ rector.php
@@ -17,12 +17,15 @@ try {
         // uncomment to reach your current PHP version
         ->withPhpSets(php83: true)
         ->withSets([
-            \RectorLaravel\Set\LaravelLevelSetList::UP_TO_LARAVEL_120,
+            \RectorLaravel\Set\LaravelSetList::LARAVEL_120,
             \RectorLaravel\Set\LaravelSetList::LARAVEL_CODE_QUALITY,
             \RectorLaravel\Set\LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
-            \RectorLaravel\Set\LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
             \RectorLaravel\Set\LaravelSetList::LARAVEL_FACTORIES,
+            \RectorLaravel\Set\LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+            \RectorLaravel\Set\LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
             \RectorLaravel\Set\LaravelSetList::LARAVEL_TYPE_DECLARATIONS,
+            \RectorLaravel\Set\LaravelSetList::LARAVEL_TESTING,
+            \RectorLaravel\Set\LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
         ])
         ->withRules([
             LivewireComponentComputedMethodToComputedAttributeRector::class,
