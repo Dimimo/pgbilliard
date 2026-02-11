5f0816b9b45c33a4aa87aea4a519352e86f2bfa9
Dimitri Mostrey <dmostrey@yahoo.com>
2026-02-11 15:11:20 +0800

    Update language files for Dutch translations

--- lang/nl/auth.php
+++ lang/nl/auth.php
@@ -1,16 +1,9 @@
 <?php
 
-return [/*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-    'failed' => 'Deze combinatie van e-mailadres en wachtwoord is niet geldig.',
-    'password' => 'Het opgegeven wachtwoord is niet geldig.',
-    'throttle' => 'Teveel gefaalde login pogingen. Probeer het over :seconds seconden nogmaals.',
+declare(strict_types=1);
+
+return [
+    'failed'   => 'Deze combinatie van e-mailadres en wachtwoord is niet geldig.',
+    'password' => 'Wachtwoord is onjuist.',
+    'throttle' => 'Te veel mislukte aanmeldpogingen. Probeer het nog eens over :seconds seconden.',
 ];
---
+++ lang/nl/plural.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'games' => '{0} <span class="text-blue-600">geen spelen</span>|{1} <span class="text-red-600">één spel</span>|[2,3] <span class="text-red-600">:count spelen</span>|[4] <span class="text-green-700">:count spelen</span>',
+    'day-games' => '{0} nog geen spelen|{1} één spel beëindigd|[2,*] :count spelen beëindigd',
+    'teams' => '{0} er zijn geen teams|{1} er is één team|[2,*] er zijn :count teams',
+];
