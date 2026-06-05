f08326b4c48ec264b2c9261b67b5a2e365af5138
Dimitri Mostrey <dmostrey@yahoo.com>
2026-06-05 15:42:36 +0800

    the results calculation shows a better format for the last played game (last column in overview)

--- app/Traits/ResultsTrait.php
+++ app/Traits/ResultsTrait.php
@@ -40,11 +40,15 @@ trait ResultsTrait
             $result = $this->startCollection();
             $result->put('team', $this->team_names->find($team_id));
             foreach ($events as $event) {
-                if (!is_null($event->score1) && !is_null($event->score2) && $event->team_2->name !== 'BYE') {
+                if (
+                    !is_null($event->score1) &&
+                    !is_null($event->score2) &&
+                    $event->team_2->name !== 'BYE'
+                ) {
                     $result->put('id', $event->id);
                     $result->put('last_game_won', false);
                     $result->put('games_played', $result->get('games_played') + 1);
-                    $result->put('last_play_date', $event->date->date->format('d/m/Y'));
+                    $result->put('last_play_date', $event->date->date->format('M jS'));
                     //team plays home
                     if ($team_id === $event->team_1->id) {
                         $result->put('played', $event->team_2);
@@ -55,21 +59,24 @@ trait ResultsTrait
                             $result->put('last_result', 'not in');
                         } else {
                             // $result->put('last_result', "$event->score1/$event->score2");
-                            $result->put('last_result', collect(['score1' => $event->score1, 'score2' => $event->score2]));
+                            $result->put(
+                                'last_result',
+                                collect(['score1' => $event->score1, 'score2' => $event->score2]),
+                            );
                         }
                         if ($event->score1 > 7) {
                             $result->put('won', $result->get('won') + 1);
                             $result->put('last_game_won', true);
                         } elseif (
                             // a fix in case of a score of 0-15 or 0-8, shouldn't be mixed up with a no show
-                            ($event->score1 > 0 && $event->score2 > 0)
-                            ||
+                            ($event->score1 > 0 && $event->score2 > 0) ||
                             ($event->score1 === 0 && $event->score2 > 7)
                         ) {
                             $result->put('lost', $result->get('lost') + 1);
                             $result->put('last_game_won', false);
                         }
-                    } //team plays as visitor
+                    }
+                    //team plays as visitor
                     elseif ($team_id === $event->team_2->id) {
                         $result->put('played', $event->team_1);
                         $result->put('for', $result->get('for') + $event->score2);
@@ -79,15 +86,17 @@ trait ResultsTrait
                             $result->put('last_result', 'not in');
                         } else {
                             //$result->put('last_result', "$event->score2/$event->score1");
-                            $result->put('last_result', collect(['score2' => $event->score1, 'score1' => $event->score2]));
+                            $result->put(
+                                'last_result',
+                                collect(['score2' => $event->score1, 'score1' => $event->score2]),
+                            );
                         }
                         if ($event->score2 > 7) {
                             $result->put('won', $result->get('won') + 1);
                             $result->put('last_game_won', true);
                         } elseif (
                             // a fix in case of a score of 0-15 or 0-8, shouldn't be mixed up with a no show
-                            ($event->score1 > 0 && $event->score2 > 0)
-                            ||
+                            ($event->score1 > 0 && $event->score2 > 0) ||
                             ($event->score2 === 0 && $event->score1 > 7)
                         ) {
                             $result->put('lost', $result->get('lost') + 1);
@@ -98,8 +107,8 @@ trait ResultsTrait
                 //HERE is a tricky one, to avoid that the nr 3 is higher ranked than the runner-up
                 // 21/05/2026 I added an extra condition in case a future game has been played already
                 elseif (
-                    ($event->team_2->name === 'BYE') &&
-                    ($result->get('games_played') <= ($max_games - 1)) &&
+                    $event->team_2->name === 'BYE' &&
+                    $result->get('games_played') <= $max_games - 1 &&
                     $event->date->date->format('Y-m-d') < $max_allowed_date_for_next_week
                 ) {
                     $result->put('games_played', $result->get('games_played') + 1);
@@ -128,7 +137,10 @@ trait ResultsTrait
             return $result;
         });
         //and sort it by percentage (success rate)
-        $results = $results->sortByDesc('percentage', SORT_NATURAL)->values()->all();
+        $results = $results
+            ->sortByDesc('percentage', SORT_NATURAL)
+            ->values()
+            ->all();
         //add the real ranking to the result object
         $rank = 1;
         foreach ($results as $key => $result) {
@@ -181,6 +193,19 @@ trait ResultsTrait
         });
     }
 
+    private function maxAllowedDateForNextWeek(): string
+    {
+        $first_date = Date::query()->where('season_id', Context::getHidden('season_id'))->first();
+        $day_of_week = \Illuminate\Support\Facades\Date::createFromFormat(
+            'Y-m-d',
+            $first_date->date->format('Y-m-d'),
+        )->dayOfWeek();
+        return \Illuminate\Support\Facades\Date::now()
+            ->next($day_of_week)
+            ->subDays(3)
+            ->format('Y-m-d');
+    }
+
     /**
      * Returns a collection, is called for the calculation of every other team in the calculation loop
      */
@@ -205,22 +230,6 @@ trait ResultsTrait
         return $collection;
     }
 
-    private function maxAllowedDateForNextWeek(): string
-    {
-        $first_date = Date::query()
-            ->where('season_id', Context::getHidden('season_id'))
-            ->first();
-        $day_of_week = \Illuminate\Support\Facades\Date::createFromFormat(
-            'Y-m-d',
-            $first_date->date->format('Y-m-d')
-        )
-            ->dayOfWeek();
-        return \Illuminate\Support\Facades\Date::now()
-            ->next($day_of_week)
-            ->subDays(3)
-            ->format('Y-m-d');
-    }
-
     /**
      * Calculates the percentages of a given score table of a team
      */
@@ -233,9 +242,16 @@ trait ResultsTrait
         // multiply the percentages with a factor for the 2 teams in the final
         $factor = 1;
         if ($result->get('finals') === 2) {
-            $result->get('last_game_won') ? $factor = 1.3 : $factor = 1.15;
+            $result->get('last_game_won') ? ($factor = 1.3) : ($factor = 1.15);
         }
 
-        return (int)number_format(floor(((($result->get('won') / $result->get('max_games')) * 100) + ($result->get('for') / ($result->get('max_games') * 15)) * 100) / 2 * $factor));
+        return (int) number_format(
+            floor(
+                ((($result->get('won') / $result->get('max_games')) * 100 +
+                    ($result->get('for') / ($result->get('max_games') * 15)) * 100) /
+                    2) *
+                    $factor,
+            ),
+        );
     }
 }
