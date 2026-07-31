4a2a29f170856d57eeb1caf4b6678ed0566d7752
Dimitri Mostrey <dmostrey@yahoo.com>
2026-07-31 19:15:08 +0800

    Volt::route() becomes Route::livewire() in Livewire 4

--- routes/auth.php
+++ routes/auth.php
@@ -2,21 +2,17 @@
 
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
-use Livewire\Volt\Volt;
 
 Route::middleware('guest')->group(function (): void {
-    Volt::route('reset-password/{token}', 'pages.auth.reset-password')
-        ->name('password.reset');
+    Route::livewire('reset-password/{token}', 'pages.auth.reset-password')->name('password.reset');
 });
 
 Route::middleware('auth')->group(function (): void {
-    Volt::route('verify-email', 'pages.auth.verify-email')
-        ->name('verification.notice');
+    Route::livewire('verify-email', 'pages.auth.verify-email')->name('verification.notice');
 
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
 
-    Volt::route('confirm-password', 'pages.auth.confirm-password')
-        ->name('password.confirm');
+    Route::livewire('confirm-password', 'pages.auth.confirm-password')->name('password.confirm');
 });
