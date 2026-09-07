cb6b329b4f7214c9541d2f820635385410bc840d
Dimitri Mostrey <dmostrey@yahoo.com>
2026-09-08 01:12:26 +0800

    added the vite ably key linked to the .env file

--- resources/js/ably.js
+++ resources/js/ably.js
@@ -4,18 +4,24 @@ import * as Ably from 'ably';
 window.Ably = Ably;
 window.Echo = new Echo({
     broadcaster: 'ably',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
 });
 
-window.Echo.connector.ably.connection.on(stateChange => {
+window.Echo.connector.ably.connection.on((stateChange) => {
     if (stateChange.current === 'connected') {
-        console.log("LOGGER:: Connection event :: ", stateChange);
-        if (stateChange.current === 'disconnected' && stateChange.reason?.code === 40142) { // key/token status expired
-            console.log("LOGGER:: Connection token expired https://help.ably.io/error/40142");
+        console.log('LOGGER:: Connection event :: ', stateChange);
+        if (stateChange.current === 'disconnected' && stateChange.reason?.code === 40142) {
+            // key/token status expired
+            console.log('LOGGER:: Connection token expired https://help.ably.io/error/40142');
         }
     }
-    if (stateChange.current === 'disconnected' || stateChange.current === 'suspended' || stateChange.current === 'failed') {
+    if (
+        stateChange.current === 'disconnected' ||
+        stateChange.current === 'suspended' ||
+        stateChange.current === 'failed'
+    ) {
         window.onfocus = function () {
             window.Echo.connect();
-        }
+        };
     }
 });
