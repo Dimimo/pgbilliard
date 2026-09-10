db821ae508aaabf8f2f94946c1f3b8f16036c4c1
Dimitri Mostrey <dmostrey@yahoo.com>
2026-09-10 12:31:20 +0800

    fix(accounts-view): reformat markup and guard nullable last_game
    - reflow paragraph blocks in accounts.blade.php to remove extra
      whitespace/line breaks introduced by blade formatting
    - temporarily comment out claim-account anchor link pending rework
    - use null-safe operator (`$user?->last_game`) to avoid runtime error
      when a player has no recorded last game date
    
    Refs: JIRA-000, accounts-view

--- resources/views/livewire/players/accounts.blade.php
+++ resources/views/livewire/players/accounts.blade.php
@@ -7,34 +7,27 @@
                 Please read if you don't have a login (account) yet!
             </span>
         </div>
-        <p class="my-3">
-            Because the database is rebuild from scratch,
-            <strong>all players</strong>
-            are linked to a
-            <strong>user</strong>
-            in the
-            <i>users</i>
-            table. To accomplish this, I had to link players to a
-            <strong>temporarily</strong>
-            user account with a bogus email address.
-        </p>
-        <p class="my-3">
-            The list underneath is build up from the
-            <i>old</i>
-            database. Mainly, only
-            <strong>captains</strong>
-            are in the list. As a result,
-            <strong>it is perfectly possible you are not in the list at all</strong>
-            . Because in recent seasons, we didn't add all the team players. You will recognize some
-            player names from the past. For better or for worse. It's the way it is.
-        </p>
-        <p class="my-3">
-            To help you to determine if you are listed here, you can see your
-            <strong>last playing date</strong>
-            and the
-            <strong>team you played for</strong>
-            .
-        </p>
+        <p class="my-3">Because the database is rebuild from scratch,
+        <strong>all players</strong>
+        are linked to a
+        <strong>user</strong>
+        in the
+        <i>users</i>
+        table. To accomplish this, I had to link players to a
+        <strong>temporarily</strong>
+        user account with a bogus email address.</p>
+        <p class="my-3">The list underneath is build up from the
+        <i>old</i>
+        database. Mainly, only
+        <strong>captains</strong>
+        are in the list. As a result,
+        <strong>it is perfectly possible you are not in the list at all</strong>
+        . Because in recent seasons, we didn't add all the team players. You will recognize some player names from the past. For better or for worse. It's the way it is.</p>
+        <p class="my-3">To help you to determine if you are listed here, you can see your
+        <strong>last playing date</strong>
+        and the
+        <strong>team you played for</strong>
+        .</p>
         <p class="my-3">
             <strong>User names have to be unique!</strong>
             For that reason alone, you should claim your account if are you in the list. There can
@@ -49,36 +42,22 @@
             </a>
             so I can fix it directly in the database.
         </p>
-        <p class="my-3">
-            If you are not in the list, simply
-            <a
-                href="{{ route('register') }}"
-                class="border border-white font-semibold text-blue-700 hover:border hover:border-blue-700 hover:bg-blue-100"
-                wire:navigate
-            >
-                create a new account
-            </a>
-            .
-        </p>
-        <p class="my-3">
-            If you are listed,
-            <strong>claim the account</strong>
-            .
-            <strong>Change your email address</strong>
-            <span class="text-red-700">
-                (this is important, so you can reset your password and receive notifications!)
-            </span>
-            and
-            <strong>change your password</strong>
-            .
-        </p>
-        <p class="my-3">
-            This page is temporarily and
-            <strong>will disappear in the near future</strong>
-            .
-            <u>Please be thoughtful!</u>
-            Claiming an account that is not yours will bring yourself in a stranger spot than me.
-        </p>
+        <p class="my-3">If you are not in the list, simply
+        <a href="{{ route('register') }}" class="border border-white font-semibold text-blue-700 hover:border hover:border-blue-700 hover:bg-blue-100" wire:navigate> create a new account </a>
+        .</p>
+        <p class="my-3">If you are listed,
+        <strong>claim the account</strong>
+        .
+        <strong>Change your email address</strong>
+        <span class="text-red-700"> (this is important, so you can reset your password and receive notifications!) </span>
+        and
+        <strong>change your password</strong>
+        .</p>
+        <p class="my-3">This page is temporarily and
+        <strong>will disappear in the near future</strong>
+        .
+        <u>Please be thoughtful!</u>
+        Claiming an account that is not yours will bring yourself in a stranger spot than me.</p>
         <p class="mt-3 text-xl">That's it!</p>
         <p>Yours, Dimitri</p>
         <p class="mx-auto mt-6 bg-blue-100 p-2 text-center italic">
@@ -105,14 +84,14 @@
                     <td class="p-2">{{ $user->name }}</td>
                     <td class="p-2">{{ $user->email }}</td>
                     <td class="p-2">
-                        <a
+                        {{--<a
                             href="{{ route('players.accounts.claim', ['user' => $user]) }}"
                             class="border border-white font-semibold text-blue-700 hover:border hover:border-blue-700 hover:bg-blue-100"
                         >
                             Claim
-                        </a>
+                        </a>--}}
                     </td>
-                    <td class="p-2">{{ $user->last_game->format('d-m-Y') }}</td>
+                    <td class="p-2">{{ $user?->last_game->format('d-m-Y') }}</td>
                     <td class="p-2">
                         {{ $user->players()->orderByDesc('team_id')->first()?->team->name }}
                     </td>
